2cae6a38f313f8e8041dc8c1f32c50c62f372ff6
Web-Dev-Master1117 <remarkable.sr1117@gmail.com>
2023-07-30 21:37:00 -0300

    first commit

---
+++ src/Layouts/LayoutMenuData.js
@@ -0,0 +1,145 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Navdata = () => {
+    const history = useNavigate();
+  
+    const [isAnalytics, setIsAnalytics] = useState(false);
+    const [isTerritory, setIsTerritory] = useState(false);
+    const [isResources, setIsResources] = useState(false);
+
+    const [iscurrentState, setIscurrentState] = useState();
+
+    function updateIconSidebar(e) {
+        if (e && e.target && e.target.getAttribute("subitems")) {
+            const ul = document.getElementById("two-column-menu");
+            const iconItems = ul.querySelectorAll(".nav-icon.active");
+            let activeIconItems = [...iconItems];
+            activeIconItems.forEach((item) => {
+                item.classList.remove("active");
+                var id = item.getAttribute("subitems");
+                if (document.getElementById(id))
+                    document.getElementById(id).classList.remove("show");
+            });
+        }
+    }
+
+    useEffect(() => {
+        document.body.classList.remove('twocolumn-panel');
+        if (iscurrentState !== 'Analytics') {
+            setIsAnalytics(false);
+        }
+        if (iscurrentState !== 'Territory') {
+            setIsTerritory(false);
+        }
+        if (iscurrentState !== 'Resources') {
+            setIsResources(false);
+        }
+    }, [
+        history,
+        iscurrentState,
+        isAnalytics,
+        isTerritory,
+        isResources
+    ]);
+
+    const menuItems = [
+        {
+            label: "Menu",
+            isHeader: true,
+        },
+        {
+            id: "analytics",
+            label: "Analytics",
+            icon: "bx bx-chart",
+            link: "/#",
+            stateVariables: isAnalytics,
+            click: function (e) {
+                e.preventDefault();
+                setIsAnalytics(!isAnalytics);
+                setIscurrentState('Analytics');
+                updateIconSidebar(e);
+            },
+            subItems: [
+                {
+                    id: "summary",
+                    label: "Summary",
+                    link: "#",
+                    parentId: "analytics",
+                },
+                {
+                    id: "sales-development",
+                    label: "Sales Development",
+                    link: "#",
+                    parentId: "analytics",
+                    childItems: [
+                        { id: 1, label: "Overview", link: "#", parentId: "analytics" },
+                        { id: 2, label: "Leaders", link: "#", parentId: "analytics" },
+                    ]
+                },
+                {
+                    id: "new-sales",
+                    label: "New Sales",
+                    link: "#",
+                    parentId: "analytics",
+                    childItems: [
+                        { id: 1, label: "Overview", link: "#", parentId: "analytics" },
+                        { id: 2, label: "Leaders", link: "#", parentId: "analytics" },
+                    ]
+                },
+                {
+                    id: 'account-management',
+                    label: 'Account Management',
+                    link: '#',
+                    parentId: 'analytics',
+                    childItems: [
+                        { id: 1, label: 'Overview', link: '#', parentId: 'analytics' },
+                        { id: 2, label: 'Leaders', link: '#', parentId: 'analytics' },
+                    ]
+                }
+            ],
+        },
+        {
+            id: "territory-mapping",
+            label: "Territory Mapping",
+            icon: "bx bx-world",
+            link: "/#",
+            click: function (e) {
+                e.preventDefault();
+                setIsTerritory(!isTerritory);
+                setIscurrentState('Territory');
+                updateIconSidebar(e);
+            },
+            stateVariables: isTerritory,
+        },
+        {
+            id: "resources",
+            label: "Resources",
+            icon: "bx bx-help-circle",
+            link: "/#",
+            click: function (e) {
+                e.preventDefault();
+                setIsResources(!isResources);
+                setIscurrentState('Resources');
+                updateIconSidebar(e);
+            },
+            stateVariables: isResources,
+            subItems: [
+                {
+                    id: "knowledge-base",
+                    label: "Knowledge Base",
+                    link: "#",
+                    parentId: "resources",
+                },
+                {
+                    id: "support",
+                    label: "Support",
+                    link: "#",
+                    parentId: "resources",
+                }
+            ]
+        },
+    ];
+    return <React.Fragment>{menuItems}</React.Fragment>;
+};
+export default Navdata;

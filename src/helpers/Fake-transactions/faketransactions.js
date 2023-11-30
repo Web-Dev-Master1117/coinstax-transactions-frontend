130653bf2d2cb6a5f345369c246d059a5fc02b6f
Web-Dev-Master1117 <remarkable.sr1117@gmail.com>
2023-11-30 16:40:48 -0300

    hostiral page \ faketransactions

---
+++ src/helpers/Fake-transactions/faketransactions.js
@@ -0,0 +1,102 @@
+import cripto1 from "../../assets/images/svg/crypto-icons/0xbtc.svg";
+import cripto2 from "../../assets/images/svg/crypto-icons/abt.svg";
+import cripto3 from "../../assets/images/svg/crypto-icons/add.svg";
+import cripto4 from "../../assets/images/svg/crypto-icons/adx.svg";
+
+import icon1 from "../../assets/images/companies/img-1.png";
+import icon2 from "../../assets/images/companies/img-2.png";
+import icon3 from "../../assets/images/companies/img-3.png";
+import icon4 from "../../assets/images/companies/img-4.png";
+
+import gif1 from "../../assets/images/nft/gif/img-1.gif";
+import gif2 from "../../assets/images/nft/gif/img-2.gif";
+import gif3 from "../../assets/images/nft/gif/img-3.gif";
+import gif4 from "../../assets/images/nft/gif/img-4.gif";
+
+export const fakeTransactions = [
+  {
+    action: "Receive",
+    img: cripto1,
+    hour: "12:00",
+    number: "0x4b8d...",
+    img2: icon1,
+    date: "12 de Mayo de 2023",
+    info: "N/A",
+    amount: "0.0001",
+    img3: gif1,
+    from: "0x4b8d...",
+  },
+  {
+    action: "Trade",
+    img: cripto2,
+    hour: "12:00",
+    number: "0x4b8d...",
+    img2: icon2,
+    date: "11 de Junio de 2023",
+    info: "11.372,40 US$",
+    amount: "0.0001",
+    img3: gif4,
+    from: "0x4b8d...",
+  },
+  {
+    action: "Mint",
+    img: cripto3,
+    hour: "12:00",
+    number: "0x4b8d...",
+    img2: icon3,
+    date: "15 de Agosto de 2023",
+    info: "N/A",
+    amount: "0.0001",
+    img3: gif2,
+    from: "0x4b8d...",
+  },
+  {
+    action: "Send",
+    img: cripto4,
+    hour: "12:00",
+    img2: icon4,
+    number: "0x4b8d...",
+    date: "04 de Noviembre de 2023",
+    info: "N/A",
+    amount: "0.0001",
+    img3: gif1,
+    from: "0x4b8d...",
+  },
+  {
+    action: "Approve",
+    img: cripto1,
+    hour: "12:00",
+    number: "0x4b8d...",
+    img2: icon2,
+    date: "3 de Enero de 2023",
+    info: "N/A",
+    amount: "0.0001",
+    img3: gif3,
+    from: "0x4b8d...",
+  },
+  {
+    action: "executed",
+    img: cripto2,
+    hour: "12:00",
+    img2: icon1,
+    number: "0x4b8d...",
+    info: "N/A",
+    date: "1 de Julio de 2023",
+    info: "4743,28 US$",
+    amount: "0.0001",
+    img3: gif2,
+    from: "0x4b8d...",
+  },
+  {
+    action: "success",
+    img: cripto3,
+    hour: "12:00",
+    number: "0x4b8d...",
+    img2: icon3,
+    date: "14 de Septiembre de 2023",
+    info: "82,25 US$",
+    amount: "0.0001",
+    img3: gif4,
+    from: "0x4b8d...",
+  },
+];
--- src/pages/DashboardInfo/components/HistorialTable.js
+++ src/pages/DashboardInfo/components/HistorialTable.js
@@ -6,14 +6,10 @@ import {
   Col,
   InputGroup,
   Collapse,
-  Card,
   CardBody,
-  AccordionBody,
-  AccordionHeader,
-  Accordion,
-  AccordionItem,
 } from "reactstrap";
-import classnames from "classnames";
+
+import { fakeTransactions } from "../../../helpers/Fake-transactions/faketransactions";
 
 const HistorialTable = () => {
   const inputRef = useRef(null);
@@ -22,200 +18,147 @@ const HistorialTable = () => {
   const toggleCollapse = (index) => {
     setOpenCollapse(openCollapse === index ? null : index);
   };
-  const [iconCol1, seticonCol1] = useState(true);
-  const [iconCol2, seticonCol2] = useState(false);
-  const [iconCol3, seticonCol3] = useState(false);
-
-  const t_iconCol1 = () => {
-    seticonCol1(!iconCol1);
-    seticonCol2(false);
-    seticonCol3(false);
-  };
-
-  const t_iconCol2 = () => {
-    seticonCol2(!iconCol2);
-    seticonCol1(false);
-    seticonCol3(false);
-  };
-
-  const t_iconCol3 = () => {
-    seticonCol3(!iconCol3);
-    seticonCol1(false);
-    seticonCol2(false);
-  };
-
-  const fakeTransactions = [
-    {
-      action: "recieved",
-      number: "0x4b8d...",
-      date: "2021-07-12",
-      amount: "0.0001",
-      from: "0x4b8d...",
-    },
-    {
-      action: "trade",
-      number: "0x4b8d...",
-      date: "2021-07-12",
-      amount: "0.0001",
-      from: "0x4b8d...",
-    },
-    {
-      action: "mint",
-      number: "0x4b8d...",
-      date: "2021-07-12",
-      amount: "0.0001",
-      from: "0x4b8d...",
-    },
-    {
-      action: "send",
-      number: "0x4b8d...",
-      date: "2021-07-12",
-      amount: "0.0001",
-      from: "0x4b8d...",
-    },
-    {
-      action: "approved",
-      number: "0x4b8d...",
-      date: "2021-07-12",
-      amount: "0.0001",
-      from: "0x4b8d...",
-    },
-    {
-      action: "executed",
-      number: "0x4b8d...",
-      date: "2021-07-12",
-      amount: "0.0001",
-      from: "0x4b8d...",
-    },
-    {
-      action: "success",
-      number: "0x4b8d...",
-      date: "2021-07-12",
-      amount: "0.0001",
-      from: "0x4b8d...",
-    },
-  ];
 
   return (
     <React.Fragment>
       <Row>
-        <div className="d-flex justify-content-start align-items-center">
-          <Button color="soft-primary" className="btn btn-sm">
+        <Col>
+          <Button color="soft-primary" size="sm">
             Transactions
           </Button>
-          <Button color="soft-primary" className="ms-2 btn btn-sm">
+          <Button color="soft-primary" size="sm" className="ms-2">
             All Assets
           </Button>
-        </div>
+        </Col>
+      </Row>
+      <Row className="mt-4">
+        <Col xxl={6}>
+          <InputGroup className="search-bar mb-3">
+            <i className="ri-search-line text-muted fs-4 position-absolute ms-2"></i>
+            <Input
+              innerRef={inputRef}
+              className="search-input form-control ps-5"
+              placeholder="Filter for direction, protocol, active, type"
+            />
+          </InputGroup>
+        </Col>
+        <Col xxl={6} className="d-flex justify-content-end">
+          <Button color="primary" size="sm">
+            Descargar CSV
+          </Button>
+        </Col>
       </Row>
       <Col xxl={12}>
-        <div className="d-flex justify-content-between mt-4">
-          <Col xxl={6}>
-            <InputGroup className="py-2 search-bar col-lg-12 col-md-12 pe-3">
-              <span
-                className="search-icon ps-2 position-absolute"
-                onClick={() => inputRef.current.focus()}
-                style={{ zIndex: 1, cursor: "text" }}
-              >
-                <i className="ri-search-line text-muted fs-3"></i>
-              </span>
-              <Input
-                innerRef={inputRef}
-                className="search-input py-2"
-                style={{ borderRadius: "5px", zIndex: 0, paddingLeft: "47px" }}
-                placeholder="Filter for direction, protocol, active, type"
-              />
-            </InputGroup>
-          </Col>
-          <Col xxl={6} className="d-flex justify-content-end">
-            <Button color="primary" className="btn btn-sm">
-              Descargar CSV
-            </Button>
-          </Col>
-        </div>
-        <Accordion
-          open={openCollapse}
-          className="border-0bg-transparent"
-          toggle={toggleCollapse}
-        >
-          {fakeTransactions.map((transaction, index) => (
-            <AccordionItem
-              key={index}
-              className="bg-transparent border-0"
-              // style={{ backgroundColor: "red " }}
+        {fakeTransactions.map((transaction, index) => (
+          <div key={index} className="align-items-center">
+            <Col
+              xxl={12}
+              className="d-flex justify-content-start pt-4 pb-1 align-items-center"
             >
-              <Col
-                xxl={12}
-                className="d-flex justify-content-start pt-4 pb-1 align-items-center"
+              <div className="d-flex justify-content-center ps-2  align-items-center ">
+                <h5 className="fw-semibold">{transaction.date}</h5>
+              </div>
+            </Col>
+            <div
+              className={`border-top border-bottom rounded${
+                openCollapse === index ? " border border-primary" : " bg-light"
+              }`}
+            >
+              <Row
+                className={`align-items-center  justify-content-between p-3 `}
+                onClick={() => toggleCollapse(index)}
+                style={{ cursor: "pointer" }}
               >
-                <div className="d-flex justify-content-center ps-2  align-items-center ">
-                  <span className="text-dark">{transaction.date}</span>
-                </div>
-              </Col>
-              <AccordionHeader
-                targetId={`${index}`}
-                className="border-0 bg-transparent"
-              >
-                <Col xxl={12}>
-                  <Row className="mt-3">
-                    <Col
-                      xxl={3}
-                      className="d-flex justify-content-center align-items-center"
-                    >
-                      <span className="text-dark">{transaction.action}</span>
-                    </Col>
-                    <Col
-                      xxl={3}
-                      className="d-flex justify-content-center align-items-center"
-                    >
-                      <span className="text-dark">{transaction.number}</span>
-                    </Col>
-                    <Col
-                      xxl={3}
-                      className="d-flex justify-content-center align-items-center"
-                    >
-                      <span className="text-dark">{transaction.amount}</span>
-                    </Col>
-                    <Col
-                      xxl={3}
-                      className="d-flex justify-content-center align-items-center"
-                    >
-                      <div className="d-flex flex-column justify-content-start align-items-center">
-                        <small>From</small>
-                        <span className="text-dark">{transaction.from}</span>
-                      </div>
-                    </Col>
-                  </Row>
+                <Col xxl={3} className="d-flex align-items-center">
+                  <img
+                    src={transaction.img}
+                    alt=""
+                    className="me-2"
+                    width={40}
+                    height={40}
+                  />
+                  <div className="d-flex flex-column text-start justify-content-end">
+                    <h5 className="fw-semibold my-0"> {transaction.action}</h5>
+                    <p className="text-start my-0">{transaction.hour}</p>
+                  </div>
                 </Col>
-              </AccordionHeader>
-              <AccordionBody
-                accordionId={`${index}`}
-                className="border-bottom border-2"
-              >
-                <div className="py-2">
-                  {/* Aquí puedes agregar el contenido detallado para cada transacción */}
-
-                  <Col xxl={6} className="">
-                    <div className="mt-3 my-0 d-flex fle-row justify-content-around">
-                      <div className="d-flex flex-column">
-                        <span className="text-mutedmb-0">Commission:</span>
-                        <span className="fw-bold">{transaction.amount}</span>
-                      </div>{" "}
-                      <div className="d-flex flex-column">
-                        <span className=" mb-0 text-muted ">
-                          Has of transaction:
-                        </span>
-                        <span className="mb-0 fw-bold">
-                          {transaction.number}
-                        </span>
-                      </div>
+                <Col
+                  xxl={3}
+                  className="d-flex justify-content-between align-items-center"
+                >
+                  <Col xxl={3} className="d-flex align-items-center">
+                    <img
+                      src={transaction.img2}
+                      alt=""
+                      className="me-2"
+                      width={40}
+                      height={40}
+                    />
+                    <div className="d-flex flex-column text-start justify-content-end">
+                      <h5 className="fw-semibold my-0">
+                        {" "}
+                        {transaction.number}
+                      </h5>
+                      <p
+                        className="text-start my-0"
+                        style={{ whiteSpace: "nowrap" }}
+                      >
+                        {" "}
+                        {transaction.info}
+                      </p>
                     </div>
                   </Col>
-                </div>
-              </AccordionBody>
-            </AccordionItem>
-          ))}
-        </Accordion>
+                  <div className="justify-content-end ">
+                    <i className="ri-arrow-right-line text-end fs-4 "></i>
+                  </div>
+                </Col>
+                <Col
+                  xxl={3}
+                  className="text-dark d-flex justify-content-center"
+                >
+                  <h5 className="fw-semibold my-0"> {transaction.amount}</h5>
+                </Col>
+                <Col xxl={3} className="d-flex align-items-center">
+                  <img
+                    src={transaction.img3}
+                    alt=""
+                    className="me-2"
+                    style={{ borderRadius: "50%" }}
+                    width={40}
+                    height={40}
+                  />
+                  <div className="d-flex flex-column text-start justify-content-end">
+                    <p className="text-start my-0">From</p>
+                    <h5 className="fw-semibold my-0"> {transaction.from}</h5>
+                  </div>
+                </Col>
+              </Row>
+              <Collapse isOpen={openCollapse === index}>
+                <CardBody
+                  onClick={() => toggleCollapse(index)}
+                  className={`ps-2 cursor-pointer ${
+                    openCollapse === index ? "border-info" : ""
+                  }`}
+                >
+                  <Col xxl={12}>
+                    <Row className="d-flex flex-row align-items-center">
+                      <Col xxl={4} className="p-2 d-flex ">
+                        <div className="p-2  mx-2 d-flex flex-column">
+                          <strong>Commission:</strong>
+                          <span className="ms-2">{transaction.amount}</span>
+                        </div>
+                        <div className="p-2 d-flex flex-column">
+                          <strong>Transaction Hash:</strong>
+                          <span className="ms-1">{transaction.number}</span>
+                        </div>
+                      </Col>
+                    </Row>
+                  </Col>
+                </CardBody>
+              </Collapse>
+            </div>
+          </div>
+        ))}
       </Col>
     </React.Fragment>
   );

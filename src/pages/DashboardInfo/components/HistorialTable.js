b470d8ef8a9cb8bdbedc1d3960abf8e1ef5f5d6b
Web-Dev-Master1117 <remarkable.sr1117@gmail.com>
2023-12-01 18:19:46 -0300

    Update dashboard navigation labels

--- src/pages/DashboardInfo/components/HistorialTable.js
+++ src/pages/DashboardInfo/components/HistorialTable.js
@@ -37,26 +37,26 @@ const HistorialTable = () => {
             <span
               className="search-icon ps-3 position-absolute"
               onClick={() => inputRef.current.focus()}
-              style={{ zIndex: 1, cursor: "text", paddingTop: ".35rem" }}
+              style={{ zIndex: 1, cursor: "text" }}
             >
               <i className="ri-search-line text-muted fs-3"></i>
             </span>
             <Input
               innerRef={inputRef}
-              className="search-input py-3 rounded"
+              className="search-input py-2 rounded"
               style={{
                 zIndex: 0,
                 paddingLeft: "47px",
               }}
-              placeholder="Filter for direction, protocol, active, type"
+              placeholder="Filter by Address, Protocol, Assets, Type"
               // value={searchTerm}
               // onChange={handleSearch}
             />
           </InputGroup>
         </Col>
-        <Col xxl={6} className="d-flex btn btn-sm py-4 justify-content-end">
+        <Col xxl={6} className="d-flex btn btn-sm py-3 justify-content-end">
           <Button color="primary" size="sm">
-            Descargar CSV
+            Download CSV
           </Button>
         </Col>
       </Row>
@@ -68,7 +68,7 @@ const HistorialTable = () => {
               className="d-flex justify-content-start pt-4 pb-1 align-items-center"
             >
               <div className="d-flex justify-content-center ps-2  align-items-center ">
-                <h5 className="fw-semibold">{transaction.date}</h5>
+                <h6 className="fw-semibold">{transaction.date}</h6>
               </div>
             </Col>
             <div
@@ -92,7 +92,7 @@ const HistorialTable = () => {
                     height={40}
                   />
                   <div className="d-flex flex-column text-start justify-content-end">
-                    <h5 className="fw-semibold my-0"> {transaction.action}</h5>
+                    <h6 className="fw-semibold my-0"> {transaction.action}</h6>
                     <p className="text-start my-0">{transaction.hour}</p>
                   </div>
                 </Col>
@@ -109,10 +109,10 @@ const HistorialTable = () => {
                       height={40}
                     />
                     <div className="d-flex flex-column text-start justify-content-end">
-                      <h5 className="fw-semibold my-0 text-success">
+                      <h6 className="fw-semibold my-0 text-success">
                         {" "}
-                        {transaction.number}
-                      </h5>
+                        {transaction.hash}
+                      </h6>
                       <p
                         className="text-start my-0"
                         style={{ whiteSpace: "nowrap" }}
@@ -127,7 +127,7 @@ const HistorialTable = () => {
                   </div>
                 </Col>
                 <Col xxl={3} className="d-flex justify-content-center">
-                  <h5 className="fw-semibold my-0"> {transaction.amount}</h5>
+                  <h6 className="fw-semibold my-0"> {transaction.fee}</h6>
                 </Col>
                 <Col xxl={3} className="d-flex align-items-center">
                   <img
@@ -140,7 +140,7 @@ const HistorialTable = () => {
                   />
                   <div className="d-flex flex-column text-start justify-content-end">
                     <p className="text-start my-0">From</p>
-                    <h5 className="fw-semibold my-0"> {transaction.from}</h5>
+                    <h6 className="fw-semibold my-0"> {transaction.from}</h6>
                   </div>
                 </Col>
               </Row>
@@ -151,17 +151,128 @@ const HistorialTable = () => {
                     openCollapse === index ? "border-info" : ""
                   }`}
                 >
+                  <Col xxl={12} lg={12} className="border-top ">
+                    {transaction.action2 && transaction.received && (
+                      <Row className="align-items-start g-0 mt-2">
+                        <Col xxl={12} className="d-flex mb-2">
+                          <Col xxl={3} className="ps-2">
+                            <strong>{transaction.action2[0].title}</strong>
+                          </Col>
+                          <Col xxl={9} className="ps-2">
+                            <span>Received</span>
+                          </Col>
+                        </Col>
+                        <Col
+                          xxl={2}
+                          className="d-flex align-items-start flex-column ps-2"
+                        >
+                          <div className="d-flex">
+                            <img
+                              src={transaction.action2[0].img}
+                              alt=""
+                              className="me-2"
+                              width={40}
+                              height={40}
+                            />
+                            <div className="d-flex flex-column text-start justify-content-start">
+                              <h6 className="fw-semibold my-0 text-success">
+                                {transaction.hash}
+                              </h6>
+                              <p
+                                className="text-start my-0"
+                                style={{ whiteSpace: "nowrap" }}
+                              >
+                                {transaction.info}
+                              </p>
+                            </div>
+                          </div>
+                        </Col>
+
+                        <Col
+                          xxl={1}
+                          className="d-flex justify-content-center align-items-center"
+                        >
+                          <div className="d-flex flex-column align-items-center">
+                            <div
+                              className="bg-dark"
+                              style={{ width: 1.5, height: 50 }}
+                            />
+                            <div
+                              style={{
+                                marginTop: "-12px",
+                                marginBottom: "-12px",
+                              }}
+                            >
+                              <i className="ri-arrow-right-circle-line text-success fs-1 mb-0 mt-0"></i>
+                            </div>
+                            <div
+                              className="bg-dark"
+                              style={{ width: 1.5, height: 60 }}
+                            />
+                          </div>
+                        </Col>
+
+                        <Col
+                          xxl={9}
+                          className="d-flex align-items-center flex-column justify-content-center"
+                        >
+                          {transaction.received.map(
+                            (received, receivedIndex) => (
+                              <div
+                                key={receivedIndex}
+                                className="d-flex align-items-center w-100 ps-2"
+                              >
+                                <img
+                                  src={received.img}
+                                  alt=""
+                                  className="me-2 rounded mb-3"
+                                  width={40}
+                                  height={40}
+                                />
+                                <h6 className="fw-semibold my-0 text-success">
+                                  {received.amount}
+                                </h6>
+                              </div>
+                            )
+                          )}
+                        </Col>
+                      </Row>
+                    )}
+                  </Col>
+
                   <Col xxl={12}>
                     <Row className="d-flex flex-row align-items-center">
-                      <Col xxl={4} className="p-2 d-flex ">
-                        <div className="p-2  mx-2 d-flex flex-column">
-                          <strong>Commission:</strong>
-                          <span className="ms-2">{transaction.amount}</span>
-                        </div>
-                        <div className="p-2 d-flex flex-column">
-                          <strong>Transaction Hash:</strong>
-                          <span className="ms-1">{transaction.number}</span>
-                        </div>
+                      <Col xxl={12} className="p-2 d-flex ">
+                        {transaction.collection && (
+                          <div className="p-2 mx-2 d-flex flex-column">
+                            <strong className="mb-1">Collection:</strong>
+                            <span>{transaction.collection}</span>
+                          </div>
+                        )}
+                        {transaction.fee && (
+                          <div className="p-2  mx-2 d-flex flex-column">
+                            <strong className="mb-1">Fee:</strong>
+                            <span>{transaction.fee}</span>
+                          </div>
+                        )}
+                        {transaction.hash && (
+                          <div className="align-items-center d-flex">
+                            <div className="p-2 d-flex flex-column">
+                              <strong>Transaction Hash:</strong>
+                              <div className="d-flex">
+                                <span className="ms-1d -flex align-items-center">
+                                  {transaction.hash}
+                                  <i className="ri-arrow-right-up-line fs-6"></i>
+                                </span>
+                              </div>
+                            </div>
+                            <div>
+                              <button className="btn btn-transparent p-0 ">
+                                <i className="ri-file-copy-line ms-2 fs-4"></i>
+                              </button>
+                            </div>
+                          </div>
+                        )}
                       </Col>
                     </Row>
                   </Col>

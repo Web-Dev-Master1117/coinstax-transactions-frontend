6b4360eb1785b038f379dd84e79c7826624b9190
Web-Dev-Master1117 <remarkable.sr1117@gmail.com>
2023-12-26 10:24:35 -0300

    Refactor HistorialTable component and add search functionality

--- src/slices/transactions/reducer.js
+++ src/slices/transactions/reducer.js
@@ -4,6 +4,7 @@ import {
   fetchPerformance,
   fetchAssets,
   fetchHistory,
+  fetchSearchHistoryTable,
 } from "./thunk";
 export const initialState = {
   transactions: [],
@@ -19,28 +20,43 @@ const TransactionsSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
+      // NFTS
       .addCase(fetchNFTS.fulfilled, (state, action) => {
         state.transactions = action.payload;
       })
       .addCase(fetchNFTS.rejected, (state, action) => {
         state.error = action.payload.error || null;
       })
+
+      // CHART
       .addCase(fetchPerformance.fulfilled, (state, action) => {
         state.performance = action.payload;
       })
       .addCase(fetchPerformance.rejected, (state, action) => {
         state.error = action.payload.error || null;
       })
+
+      // ASSETS
       .addCase(fetchAssets.fulfilled, (state, action) => {
         state.assets = action.payload;
       })
       .addCase(fetchAssets.rejected, (state, action) => {
         state.error = action.payload.error || null;
       })
+
+      // HISTORY
       .addCase(fetchHistory.fulfilled, (state, action) => {
         state.history = action.payload;
       })
       .addCase(fetchHistory.rejected, (state, action) => {
+        state.error = action.payload.error || null;
+      })
+
+      // SEARCH HISTORY TABLE
+      .addCase(fetchSearchHistoryTable.fulfilled, (state, action) => {
+        state.history = action.payload;
+      })
+      .addCase(fetchSearchHistoryTable.rejected, (state, action) => {
         state.error = action.payload.error || null;
       });
   },

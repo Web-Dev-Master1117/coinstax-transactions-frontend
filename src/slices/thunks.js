bd3f1fbed7dc2d00f19dc285be5eb6952725fae6
Web-Dev-Master1117 <remarkable.sr1117@gmail.com>
2024-02-04 08:42:10 -0300

    - Implemented linter (eslint + prettier)

--- src/slices/thunks.js
+++ src/slices/thunks.js
@@ -1,54 +1,51 @@
 // Front
-export * from "./layouts/thunk";
+export * from './layouts/thunk';
 
 // Authentication
-export * from "./auth/login/thunk";
-export * from "./auth/register/thunk";
-export * from "./auth/forgetpwd/thunk";
-export * from "./auth/profile/thunk";
+export * from './auth/login/thunk';
+export * from './auth/register/thunk';
+export * from './auth/forgetpwd/thunk';
+export * from './auth/profile/thunk';
 
 //Calendar
-export * from "./calendar/thunk";
+export * from './calendar/thunk';
 //Chat
-export * from "./chat/thunk";
+export * from './chat/thunk';
 //Ecommerce
-export * from "./ecommerce/thunk";
+export * from './ecommerce/thunk';
 
 //Crypto
-export * from "./crypto/thunk";
+export * from './crypto/thunk';
 
 //Crm
-export * from "./crm/thunk";
+export * from './crm/thunk';
 
 //Invoice
-export * from "./invoice/thunk";
-
+export * from './invoice/thunk';
 
 // Dashboard Analytics
-export * from "./dashboardAnalytics/thunk";
+export * from './dashboardAnalytics/thunk';
 
 // Dashboard CRM
-export * from "./dashboardCRM/thunk";
+export * from './dashboardCRM/thunk';
 
 // Dashboard Ecommerce
-export * from "./dashboardEcommerce/thunk";
+export * from './dashboardEcommerce/thunk';
 
 // Dashboard Cryto
-export * from "./dashboardCrypto/thunk";
+export * from './dashboardCrypto/thunk';
 
 // Dashboard Cryto
-export * from "./dashboardProject/thunk";
+export * from './dashboardProject/thunk';
 
 // Dashboard NFT
-export * from "./dashboardNFT/thunk";
-
+export * from './dashboardNFT/thunk';
 
 // File Manager
-export * from "./fileManager/thunk";
-
+export * from './fileManager/thunk';
 
 // Jobs
-export * from "./jobs/thunk";
+export * from './jobs/thunk';
 
 // API Key
-export * from "./apiKey/thunk";
+export * from './apiKey/thunk';

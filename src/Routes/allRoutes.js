0de674ccd6438268d87a6adf3a5363b23d28e84c
Web-Dev-Master1117 <remarkable.sr1117@gmail.com>
2024-02-08 15:14:21 -0300

    Update import statements and fix styling in DashboardHome and reducer.js files

--- src/Routes/allRoutes.js
+++ src/Routes/allRoutes.js
@@ -1,43 +1,65 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
+import React from 'react';
+import { Navigate } from 'react-router-dom';
 
 //Dashboard
-import DashboardEcommerce from "../pages/DashboardEcommerce";
+import DashboardEcommerce from '../pages/DashboardEcommerce';
 
 //login
-import ForgetPasswordPage from "../pages/Authentication/ForgetPassword";
-import Login from "../pages/Authentication/Login";
-import Logout from "../pages/Authentication/Logout";
-import Register from "../pages/Authentication/Register";
+import ForgetPasswordPage from '../pages/Authentication/ForgetPassword';
+import Login from '../pages/Authentication/Login';
+import Logout from '../pages/Authentication/Logout';
+import Register from '../pages/Authentication/Register';
 
 // User Profile
-import UserProfile from "../pages/Authentication/user-profile";
-import DashboardInfo from "../pages/DashboardInfo/DashboardInfo";
+import UserProfile from '../pages/Authentication/user-profile';
+import DashboardInfo from '../pages/DashboardInfo/DashboardInfo';
 
 const authProtectedRoutes = [
-  { path: "/dashboard", component: <DashboardInfo /> },
-  { path: "/address/:address/:type", component: <DashboardInfo /> },
-  { path: "/index", component: <DashboardEcommerce /> },
+  { path: '/dashboard', component: <DashboardInfo /> },
+  { path: '/address/:address/:type', component: <DashboardInfo /> },
+  { path: '/index', component: <DashboardEcommerce /> },
 
   //User Profile
-  { path: "/profile", component: <UserProfile /> },
+  { path: '/profile', component: <UserProfile /> },
 
   // this route should be at the end of all other routes
   // eslint-disable-next-line react/display-name
   {
-    path: "/",
+    path: '/',
     exact: true,
     component: <Navigate to="/dashboard" />,
   },
-  { path: "*", component: <Navigate to="/dashboard" /> },
+  { path: '*', component: <Navigate to="/dashboard" /> },
 ];
 
 const publicRoutes = [
   // Authentication Page
-  { path: "/logout", component: <Logout /> },
-  { path: "/login", component: <Login /> },
-  { path: "/forgot-password", component: <ForgetPasswordPage /> },
-  { path: "/register", component: <Register /> },
+  { path: '/logout', component: <Logout /> },
+  { path: '/login', component: <Login /> },
+  { path: '/forgot-password', component: <ForgetPasswordPage /> },
+  { path: '/register', component: <Register /> },
 ];
 
-export { authProtectedRoutes, publicRoutes };
+const allRoutes = [
+  { path: '/logout', component: <Logout /> },
+  { path: '/login', component: <Login /> },
+  { path: '/forgot-password', component: <ForgetPasswordPage /> },
+  { path: '/register', component: <Register /> },
+  { path: '/dashboard', component: <DashboardInfo /> },
+  { path: '/address/:address/:type', component: <DashboardInfo /> },
+  { path: '/index', component: <DashboardEcommerce /> },
+
+  //User Profile
+  { path: '/profile', component: <UserProfile /> },
+
+  // this route should be at the end of all other routes
+  // eslint-disable-next-line react/display-name
+  {
+    path: '/',
+    exact: true,
+    component: <Navigate to="/dashboard" />,
+  },
+  { path: '*', component: <Navigate to="/dashboard" /> },
+];
+
+export { allRoutes, authProtectedRoutes, publicRoutes };

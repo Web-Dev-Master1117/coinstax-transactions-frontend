f0edbf633bc54b5d0bc304b4e6992f03f7e8a4f8
Web-Dev-Master1117 <remarkable.sr1117@gmail.com>
2024-02-19 14:40:12 -0300

    Refactor Footer and DashboardHome components

--- src/pages/DashboardHome/DashboardHome.js
+++ src/pages/DashboardHome/DashboardHome.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Container } from 'reactstrap';
 
 const DashboardHome = () => {
   return (
-    <div
-      // style={{ height: '100vh' }}
-      className="d-flex page-content justify-content-center align-items-start page-content"
-    >
-      <h1>Search for your wallet address to get started</h1>
-    </div>
+    <React.Fragment>
+      <Container fluid className="page-content" style={{ height: '100vh' }}>
+        <div className="d-flex justify-content-center align-items-start">
+          <h1>Search for your wallet address to get started</h1>
+        </div>
+      </Container>
+    </React.Fragment>
   );
 };
 

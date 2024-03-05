beeb85f0d2c402313712602982bd882f84c3a1df
Web-Dev-Master1117 <remarkable.sr1117@gmail.com>
2024-03-05 11:21:33 -0300

    Add blockchain contract fetching and display functionality

--- src/pages/DashboardBlockchainContracts/DashboardBlockchainContracts.js
+++ src/pages/DashboardBlockchainContracts/DashboardBlockchainContracts.js
@@ -1,11 +1,51 @@
-import React from 'react';
-import { Table } from 'reactstrap';
+import React, { useEffect, useState } from 'react';
+import { Spinner, Table } from 'reactstrap';
+import { fetchBlockchainContracts } from '../../slices/blockchainContracts/thunk';
+import { useDispatch } from 'react-redux';
+import { formatIdTransaction } from '../../utils/utils';
 
 const DashboardBlockchainContracts = () => {
+  const dispatch = useDispatch();
+
+  const [loading, setLoading] = useState(false);
+  const [contracts, setContracts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const getBlockchainContracts = async () => {
+    try {
+      setLoading(true);
+      const response = await dispatch(
+        fetchBlockchainContracts({ blockchain: 'ethereum', page: currentPage }),
+      );
+      console.log(response);
+      setContracts(response.payload);
+      setLoading(false);
+    } catch (error) {
+      setLoading(true);
+      console.error('Error fetching blockchain contracts', error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getBlockchainContracts();
+  }, []);
+
+  if (loading || !contracts) {
+    return (
+      <div
+        className="d-flex justify-content-center align-items-center"
+        style={{ height: '100vh' }}
+      >
+        <Spinner style={{ width: '4rem', height: '4rem' }} />
+      </div>
+    );
+  }
+
   return (
     <React.Fragment>
-      <div className="page-content h-100vh">
-        <Table>
+      <div className="page-content">
+        <Table hover shadow responsive>
           <thead>
             <tr>
               <th>ID</th>
@@ -19,7 +59,46 @@ const DashboardBlockchainContracts = () => {
               <th>Actions</th>
             </tr>
           </thead>
-          <tbody>{/* Add table rows here */}</tbody>
+          <tbody>
+            {contracts &&
+              contracts.map((contract) => (
+                <tr key={contract.Id}>
+                  <td className="align-middle">{contract.Id}</td>
+                  <td className="align-middle">{contract.Blockchain}</td>
+                  <td className="align-middle">
+                    {formatIdTransaction(contract.Address, 4, 4)}
+                  </td>
+                  <td className="align-middle">{contract.Type}</td>
+                  <td className="align-middle">
+                    {formatIdTransaction(contract.Name, 4, 4)}
+                  </td>
+                  <td className="align-middle">
+                    <img
+                      src={contract.Logo || contract.Blockchain}
+                      alt={contract.Logo}
+                      className="ps-0 rounded"
+                      width={35}
+                      height={35}
+                      onError={(e) => {
+                        e.target.onerror = null;
+                        e.target.style.display = 'none';
+                        const container = e.target.parentNode;
+                        const textNode = document.createElement('div');
+                        textNode.textContent = contract.Blockchain;
+                        textNode.className = 'currency-placeholder';
+                        container.appendChild(textNode);
+                      }}
+                    />
+                  </td>
+                  <td className="align-middle">{contract.Symbol}</td>
+                  <td className="align-middle">{contract.TrustedState}</td>
+                  <td className="align-middle">
+                    <i className="ri-pencil-line text-primary cursor-pointer border rounded border-light p-2"></i>
+                    <i className="ri-delete-bin-line text-danger cursor-pointer border rounded border-light p-2 ms-2"></i>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
         </Table>
       </div>
     </React.Fragment>
---
+++ src/slices/blockchainContracts/thunk.js
@@ -0,0 +1,20 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+const API_BASE = process.env.REACT_APP_API_URL_BASE;
+
+export const fetchBlockchainContracts = createAsyncThunk(
+  'blockchainContracts/fetchBlockchainContracts',
+  async ({ blockchain, page }, { rejectWithValue }) => {
+    try {
+      const response = await fetch(
+        `${API_BASE}/admin/contracts/${blockchain}?page=${page}`,
+      );
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  },
+);

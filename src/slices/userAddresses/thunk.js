eb779abb2119daaf6942f035cf750dc8e1778943
Web-Dev-Master1117 <remarkable.sr1117@gmail.com>
2024-03-12 15:50:09 -0300

    Add refreshAllTransactions functionality to DashboardUserAddresses

--- src/slices/userAddresses/thunk.js
+++ src/slices/userAddresses/thunk.js
@@ -21,3 +21,21 @@ export const getUserAddresses = createAsyncThunk(
     }
   },
 );
+
+export const refreshAllTransactions = createAsyncThunk(
+  'userAddresses/refreshAllTransactions',
+  async ({ blockchain, address }, { rejectWithValue }) => {
+    try {
+      const response = await fetch(
+        `${API_BASE}/admin/addresses/${blockchain}/${address}/refresh`,
+      );
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  },
+);

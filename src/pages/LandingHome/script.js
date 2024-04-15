f82cd0cd8f4174468942c822865adaeff3b1b1e1
Web-Dev-Master1117 <remarkable.sr1117@gmail.com>
2024-04-15 16:35:24 -0300

    Landing home to html

--- src/pages/LandingHome/script.js
+++ src/pages/LandingHome/script.js
@@ -75,5 +75,20 @@ document.addEventListener('DOMContentLoaded', function () {
     },
   });
 
+  var acc = document.getElementsByClassName('accordion');
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener('click', function () {
+      this.classList.toggle('active');
+      var panel = this.nextElementSibling;
+      if (panel.style.display === 'block') {
+        panel.style.display = 'none';
+      } else {
+        panel.style.display = 'block';
+      }
+    });
+  }
+
   document.getElementById('back-to-top').addEventListener('click', toTop);
 });

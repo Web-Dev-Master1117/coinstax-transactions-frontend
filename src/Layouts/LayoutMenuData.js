7fbb300e03f1ab8ecd8d3d0a1bc81e39bf1e49a6
Web-Dev-Master1117 <remarkable.sr1117@gmail.com>
2024-04-16 11:40:02 -0300

    Fix  bug items side bar

--- src/Layouts/LayoutMenuData.js
+++ src/Layouts/LayoutMenuData.js
@@ -1,192 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const Navdata = () => {
-  const [iscurrentState, setIscurrentState] = useState('Home');
-
-  const { user } = useSelector((state) => state.auth);
-
-  const { assets, transactions, performance } = useSelector(
-    (state) => state.fetchData,
-  );
-
-  const isUnsupported =
-    assets.unsupported || transactions.unsupported || performance.unsupported;
-
   const { address } = useParams();
+  const navigate = useNavigate();
+  const { user } = useSelector((state) => state.auth);
+  const { fetchData } = useSelector((state) => ({
+    fetchData: state.fetchData,
+  }));
 
   const [addressSearched, setAddressSearched] = useState('');
+  const [isUnsupported, setIsUnsupported] = useState(false);
 
   useEffect(() => {
-    if (address) {
+    if (address && address !== addressSearched) {
       setAddressSearched(address);
-    }
-    if (isUnsupported) {
-      setAddressSearched('');
     }
   }, [address]);
 
-  // function updateIconSidebar(e) {
-  //   if (e && e.target && e.target.getAttribute("subitems")) {
-  //     const ul = document.getElementById("two-column-menu");
-  //     const iconItems = ul.querySelectorAll(".nav-icon.active");
-  //     let activeIconItems = [...iconItems];
-  //     activeIconItems.forEach((item) => {
-  //       item?.classList?.remove("active");
-  //       var id = item.getAttribute("subitems");
-  //       if (document.getElementById(id))
-  //         document.getElementById(id)?.classList?.remove("show");
-  //     });
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   document.body?.classList?.remove("twocolumn-panel");
-  //   // if (iscurrentState !== "Analytics") {
-  //   //   setIsAnalytics(false);
-  //   // }
-  //   // if (iscurrentState !== "Resources") {
-  //   //   setIsResources(false);
-  //   // }
-  //   // if (iscurrentState !== "Summary") {
-  //   //   setIsSummary(false);
-  //   // }
-  //   // if (iscurrentState !== "Sales Development") {
-  //   //   setIsSalesDevelopment(false);
-  //   // }
-  //   // if (iscurrentState === "Territory") {
-  //   //   history("/territory-mapping");
-  //   //   document.body?.classList?.add("twocolumn-panel");
-  //   // }
-  // }, [history, iscurrentState, isAnalytics, isResources, isSummary]);
-
-  const isAdmin = user;
-
-  const dashboardLink = isUnsupported
-    ? '/'
-    : addressSearched
-      ? `/address/${addressSearched}`
-      : '/';
-
-  const adminMenuItems = [
-    {
-      id: 'home',
-      label: 'Home',
-      icon: 'bx bx-home',
-      link: `${dashboardLink}`,
-      click: function (e) {
-        e.preventDefault();
-        setIscurrentState('Home');
-      },
-    },
-
-    {
-      id: 'assets',
-      label: 'Assets',
-      icon: 'bx bx-coin-stack',
-      link: `/address/${addressSearched}/assets`,
-      click: function (e) {
-        e.preventDefault();
-        setIscurrentState('Assets');
-      },
-    },
-    {
-      id: 'nfts',
-      label: 'NFTs',
-      icon: 'bx bx-coin',
-      link: `/address/${addressSearched}/nfts`,
-      click: function (e) {
-        e.preventDefault();
-        setIscurrentState('nfts');
-      },
-    },
-    {
-      id: 'transactions',
-      label: 'Transactions',
-      icon: 'bx bx-transfer',
-      link: `/address/${addressSearched}/history`,
-      click: function (e) {
-        e.preventDefault();
-        setIscurrentState('Transactions');
-      },
-    },
-    {
-      label: `Admin`,
-      isHeader: true,
-    },
-
-    {
-      id: 'blockchain',
-      label: 'Blockchain Contracts',
-      icon: ' bx bx-link fs-3',
-      link: '/blockchain-contracts',
-      click: function (e) {
-        e.preventDefault();
-        setIscurrentState('Investment');
-      },
-    },
-    {
-      id: 'userAddresses',
-      label: 'User Addresses',
-      icon: ' bx bx-user fs-3',
-      link: '/user-addresses',
-      click: function (e) {
-        e.preventDefault();
-        setIscurrentState('Investment');
-      },
-    },
-  ];
-
-  const userMenuItems = [
-    {
-      id: 'home',
-      label: 'Home',
-      icon: 'bx bx-home',
-      link: `${dashboardLink}`,
-      click: function (e) {
-        e.preventDefault();
-        setIscurrentState('Home');
-      },
-    },
-    {
-      id: 'assets',
-      label: 'Assets',
-      icon: 'bx bx-coin-stack',
-      link: `/address/${addressSearched}/assets`,
-      click: function (e) {
-        e.preventDefault();
-        setIscurrentState('Assets');
-      },
-    },
-    {
-      id: 'nfts',
-      label: 'NFTs',
-      icon: 'bx bx-coin',
-      link: `/address/${addressSearched}/nfts`,
-      click: function (e) {
-        e.preventDefault();
-        setIscurrentState('nfts');
-      },
-    },
-    {
-      id: 'transactions',
-      label: 'Transactions',
-      icon: 'bx bx-transfer',
-      link: `/address/${addressSearched}/history`,
-      click: function (e) {
-        e.preventDefault();
-        setIscurrentState('Transactions');
-      },
-    },
-  ];
+  useEffect(() => {
+    const { assets, transactions, performance } = fetchData;
+    setIsUnsupported(
+      assets.unsupported || transactions.unsupported || performance.unsupported,
+    );
+  }, [fetchData]);
 
   const filterMenuItems = (menuItems) => {
-    if (
-      !addressSearched ||
-      isUnsupported
-      // && !contractAddress
-    ) {
+    if (isUnsupported) {
       return menuItems.filter(
         (item) =>
           item.id !== 'assets' &&
@@ -196,10 +37,66 @@ const Navdata = () => {
     }
     return menuItems;
   };
-  const filteredAdminMenuItems = filterMenuItems(adminMenuItems);
-  const filteredUserMenuItems = filterMenuItems(userMenuItems);
 
-  const menuItems = isAdmin ? filteredAdminMenuItems : filteredUserMenuItems;
-  return <React.Fragment>{menuItems}</React.Fragment>;
+  const createMenuItem = (id, label, icon, page) => ({
+    id,
+    label,
+    icon,
+    link: addressSearched ? `/address/${addressSearched}/${page}` : '',
+    click: function (e) {
+      e.preventDefault();
+      navigate(link);
+      setIscurrentState(label);
+    },
+  });
+  const createMenuItemAdmin = (id, label, icon, page) => ({
+    id,
+    label,
+    icon,
+    link: `/${page}`,
+    click: function (e) {
+      e.preventDefault();
+      navigate(link);
+      setIscurrentState(label);
+    },
+  });
+
+  const createMenuHeader = (label) => ({
+    id: label.toLowerCase().replace(' ', '-'),
+    label,
+    isHeader: true,
+  });
+
+  let allMenuItems = [
+    createMenuItem('home', 'Home', 'bx bx-home', ''),
+    createMenuItem('assets', 'Assets', 'bx bx-coin-stack', 'assets'),
+    createMenuItem('nfts', 'NFTs', 'bx bx-coin', 'nfts'),
+    createMenuItem('transactions', 'Transactions', 'bx bx-transfer', 'history'),
+  ];
+
+  if (user) {
+    allMenuItems.push(createMenuHeader('Admin'));
+    allMenuItems.push(
+      createMenuItemAdmin(
+        'blockchain',
+        'Blockchain Contracts',
+        'bx bx-link fs-3',
+        'blockchain-contracts',
+      ),
+    );
+    allMenuItems.push(
+      createMenuItemAdmin(
+        'userAddresses',
+        'User Addresses',
+        'bx bx-user fs-3',
+        'user-addresses',
+      ),
+    );
+  }
+
+  const filteredMenuItems = filterMenuItems(allMenuItems);
+
+  return <div>{filteredMenuItems}</div>;
 };
+
 export default Navdata;

02e365a25c620ba701d01486bf469dcc4914b512
Web-Dev-Master1117 <remarkable.sr1117@gmail.com>
2024-04-22 12:27:45 -0300

    Add Ethereum icon in DashboardTokens.js

--- src/pages/DashboardTokens/DashboardTokens.js
+++ src/pages/DashboardTokens/DashboardTokens.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import ChartTokens from './components/ChartTokens';
 import { Col } from 'reactstrap';
+import ethIcon from '../../assets/images/svg/crypto-icons/eth.svg';
 
 const DashboardTokens = () => {
   const [tokenName, setTokenName] = useState('Ethereum');
@@ -8,10 +9,17 @@ const DashboardTokens = () => {
   return (
     <React.Fragment>
       <div className="page-content ">
-        <div className="mt-5 mb-4">
-          <h1>{tokenName}</h1>
+        <div className="mt-5 ">
+          <div className="d-flex align-items-center">
+            <img src={ethIcon} alt="Ethereum" className="icon-lg me-2" />
+            <span className="fs-4">ETH</span>
+          </div>
+          <h1 className="d-flex align-items-center mt-3 mb-4">
+            {tokenName}
+            <i className="mdi mdi-check-decagram ms-2 fs-2 text-primary"></i>
+          </h1>
         </div>
-        <Col className="my-5 col-12">
+        <Col className="col-12">
           <ChartTokens />
         </Col>
       </div>

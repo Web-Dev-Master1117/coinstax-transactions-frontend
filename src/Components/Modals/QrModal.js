b888e3684f806cc735d61d3ed1377d2646b786bd
Web-Dev-Master1117 <remarkable.sr1117@gmail.com>
2024-05-08 12:08:47 -0300

    chore: Update address copy text

--- src/Components/Modals/QrModal.js
+++ src/Components/Modals/QrModal.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { Modal, ModalHeader, ModalBody, Button } from "reactstrap";
-import QRCode from "qrcode.react";
+import React from 'react';
+import { Modal, ModalHeader, ModalBody, Button } from 'reactstrap';
+import QRCode from 'qrcode.react';
 
 const QrModal = ({ showQrModal, toggleQrModal, addressTitle }) => {
   return (
@@ -13,23 +13,23 @@ const QrModal = ({ showQrModal, toggleQrModal, addressTitle }) => {
         <div className="mb-4 border-bottom">
           <h3 className="">QR Code</h3>
         </div>
-        <p className="fs-5" style={{ color: "#ff9d1c" }}>
+        {/* <p className="fs-5" style={{ color: "#ff9d1c" }}>
           Assets can only be sent within the same network
-        </p>
+        </p> */}
         <div className="d-inline-block bg-white p-2 rounded my-2">
           <QRCode className="p-1" value={addressTitle} size={256} level="H" />
         </div>
         <div className="bg-soft-dark rounded py-1 my-3">
-          <p className="my-0">{addressTitle}</p>
+          <p className="my-0">ethereum:{addressTitle}</p>
         </div>
         <div className="border-top pt-4 pb-1">
           <Button
             size="md"
             color="soft-light"
-            style={{ borderRadius: "10px", border: ".5px solid grey" }}
+            style={{ borderRadius: '10px', border: '.5px solid grey' }}
             onClick={toggleQrModal}
           >
-            <span className="mx-5 text-dark  fs-5">Cancel</span>
+            <span className="mx-5 text-dark  fs-5">Close</span>
           </Button>
         </div>
       </ModalBody>

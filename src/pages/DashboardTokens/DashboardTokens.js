8d11eb7b439158be54447b4979774561edffd65a
Web-Dev-Master1117 <remarkable.sr1117@gmail.com>
2024-05-09 11:05:43 -0300

    Fix About component layout

--- src/pages/DashboardTokens/DashboardTokens.js
+++ src/pages/DashboardTokens/DashboardTokens.js
@@ -18,7 +18,10 @@ const DashboardTokens = () => {
 
   const [loading, setLoading] = useState(false);
 
+  const [loadingChart, setLoadingChart] = useState(false);
+
   const [data, setData] = useState();
+  const [address, setAddress] = useState();
 
   const dispatch = useDispatch();
 
@@ -27,17 +30,26 @@ const DashboardTokens = () => {
   const fetchToken = async () => {
     try {
       setLoading(true);
+      setLoadingChart(true);
       const response = await dispatch(fetchCoingeckoId({ coingeckoId: token }));
 
-      if (response.payload) {
-        setData(response.payload);
+      const res = response.payload;
+
+      if (res) {
+        setData(res);
+        setAddress(res.platforms?.ethereum);
       } else {
+        if (!address) {
+          navigate('/');
+        }
         navigate('/');
       }
+      setLoadingChart(false);
       setLoading(false);
     } catch (error) {
       console.error(error);
       setLoading(false);
+      setLoadingChart(false);
     }
   };
   useEffect(() => {
@@ -47,56 +59,62 @@ const DashboardTokens = () => {
   // if (!data && !loading) {
   //   navigate('/');
   // }
-  if (loading) {
-    <div
-      style={{
-        height: '100vh',
-      }}
-    >
-      <Spinner color="primary" className="m-2" />;
-    </div>;
-  }
 
   return (
     <React.Fragment>
       <div className="page-content ">
-        <div className="mt-5 ">
-          <div className="d-flex align-items-center">
-            <img src={data?.image} alt="Ethereum" className="icon-lg me-2" />
-            <span className="fs-4">{data?.symbol}</span>
+        {loading ? (
+          <div
+            className="d-flex mt-5 justify-content-center align-items-center h-100vh"
+            // style={{ height: !isDashboardPage ? '50vh' : '40vh' }}
+          >
+            <Spinner style={{ width: '4rem', height: '4rem' }} />
           </div>
-          <h1 className="d-flex align-items-center mt-3 mb-4">
-            {data?.name}
-            {data?.verified ? (
-              <i className="mdi mdi-check-decagram ms-2 fs-2 text-primary"></i>
-            ) : null}
-          </h1>
-        </div>
-        <Col className="col-12 mb-5">
-          <PerformanceChart
-            address={token}
-            setIsUnsupported={false}
-            loading={loading}
-            setLoading={setLoading}
-          />
+        ) : (
+          <>
+            <div className="mt-5 ">
+              <div className="d-flex align-items-center">
+                <img
+                  src={data?.image}
+                  alt="Ethereum"
+                  className="icon-lg me-2"
+                />
+                <span className="fs-4">{data?.symbol}</span>
+              </div>
+              <h1 className="d-flex align-items-center mt-3 mb-4">
+                {data?.name}
+                {data?.verified ? (
+                  <i className="mdi mdi-check-decagram ms-2 fs-2 text-primary"></i>
+                ) : null}
+              </h1>
+            </div>
+            <Col className="col-12 mb-3">
+              <PerformanceChart
+                address={address}
+                setIsUnsupported={false}
+                loading={loadingChart}
+                setLoading={setLoadingChart}
+              />
 
-          <WalletCard />
-        </Col>
-        <Col className="col-12 my-5">
-          <Stats />
-        </Col>
-        <Col className="col-12 my-5">
-          <About description={data?.description} name={data?.name} />
-        </Col>
-        <Col className="col-12 my-5">
-          <Tags />
-        </Col>
-        <Col className="col-12 my-5">
-          <Explorers />
-        </Col>
-        <Col className="col-12 my-5">
-          <History />
-        </Col>
+              {/* <WalletCard /> */}
+            </Col>
+            <Col className="col-12 my-3">
+              <Stats stats={data.marketData} />
+            </Col>
+            <Col className="col-12 my-3">
+              <About description={data?.description} name={data?.name} />
+            </Col>
+            {/* <Col className="col-12 my-5">
+              <Tags />
+            </Col>
+            <Col className="col-12 my-5">
+              <Explorers />
+            </Col>
+            <Col className="col-12 my-5">
+              <History />
+            </Col> */}
+          </>
+        )}
       </div>
     </React.Fragment>
   );

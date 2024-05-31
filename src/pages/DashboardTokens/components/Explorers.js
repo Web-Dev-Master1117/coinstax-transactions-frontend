383915ea9fe02020f959457d22b32561fbcc66c5
Web-Dev-Master1117 <remarkable.sr1117@gmail.com>
2024-05-31 11:27:45 -0300

    Add images and corresponding links to token platforms

--- src/pages/DashboardTokens/components/Explorers.js
+++ src/pages/DashboardTokens/components/Explorers.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { copyToClipboard, formatIdTransaction } from '../../../utils/utils';
+import { Link } from 'react-router-dom';
 
 const Explorers = ({ platforms }) => {
   const [copiedKey, setCopiedKey] = useState(null);
@@ -7,58 +8,110 @@ const Explorers = ({ platforms }) => {
     {
       image: 'https://chain-icons.s3.amazonaws.com/zora',
       name: 'Zora',
+      key: 'zora',
+      base_url: 'https://zora.co/',
     },
-    {
-      image: 'https://chain-icons.s3.amazonaws.com/chainlist/324',
-      name: 'zkSync Era',
-    },
+
     {
       image: 'https://chain-icons.s3.amazonaws.com/scroll.png',
       name: 'Scroll',
+      key: 'scroll',
+      base_url: 'https://scrollscan.com/token',
     },
     {
-      image: 'https://chain-icons.s3.amazonaws.com/chainlist/1101',
+      image: 'https://chain-icons.s3.amazonaws.com/polygon.png',
       name: 'Polygon',
       key: 'polygon-pos',
       base_url: 'https://polygonscan.com/address/',
     },
     {
+      name: 'Polygon zkevm',
+      image: 'https://chain-icons.s3.amazonaws.com/chainlist/1101',
+      key: 'polygon-zkevm',
+      base_url: 'https://zkevm.polygonscan.com/token',
+    },
+    {
       image: 'https://chain-icons.s3.amazonaws.com/optimism.png',
       name: 'Optimism',
+      key: 'optimism',
+      base_url: 'https://optimistic.etherscan.io/token',
+    },
+    {
+      name: 'zkSync Era',
+      image: 'https://chain-icons.s3.amazonaws.com/chainlist/324',
+      key: 'zksync',
+      base_url: 'https://era.zksync.network/',
     },
     {
       image: 'https://chain-icons.s3.amazonaws.com/chainlist/59144',
       name: 'Linea',
+      key: 'linea',
+      base_url: 'https://lineascan.build/token/',
     },
     {
       image: 'https://chain-icons.s3.amazonaws.com/ethereum.png',
       name: 'Ethereum',
       key: 'ethereum',
-      base_url: 'https://etherscan.io/address/',
+      base_url: 'https://etherscan.io/token',
     },
     {
       image: 'https://chain-icons.s3.amazonaws.com/chainlist/81457',
       name: 'Blast',
+      key: 'blast',
     },
     {
       image: 'https://chain-icons.s3.amazonaws.com/chainlist/8453',
       name: 'Base',
+      key: 'base',
+      base_url: 'https://basescan.org/token',
     },
     {
       image: 'https://chain-icons.s3.amazonaws.com/aurora.png',
       name: 'Aurora',
+      key: 'aurora',
+      base_url: 'https://explorer.aurora.dev/token',
+    },
+    {
+      name: 'Tron',
+      image: 'https://cdn-icons-png.flaticon.com/512/7016/7016547.png',
+      key: 'tron',
+      // base_url: 'https://tronscan.org/#/token/',
+    },
+    {
+      name: 'Open Network',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/The_open_network_logo.svg/640px-The_open_network_logo.svg.png',
+      key: 'the-open-network',
+    },
+    {
+      name: 'Solana',
+      image: 'https://chain-icons.s3.amazonaws.com/solana.png',
+      key: 'solana',
+      base_url: 'https://explorer.solana.com/address/',
+    },
+    {
+      name: 'Near Protocol',
+      image:
+        'https://static.vecteezy.com/system/resources/thumbnails/024/093/143/small_2x/near-protocol-near-glass-crypto-coin-3d-illustration-free-png.png',
+      key: 'near-protocol',
     },
     {
       image: 'https://chain-icons.s3.amazonaws.com/chainlist/3776',
       name: 'Astar',
+      key: 'astar',
+      // base_url: '',
     },
     {
       image: 'https://chain-icons.s3.amazonaws.com/arbitrum.png',
       name: 'Arbitrum',
+      key: 'arbitrum',
+      base_url: 'https://arbiscan.io/token',
     },
     {
-      name: 'Binance',
+      name: 'BNB Chain',
       image: 'https://chain-icons.s3.amazonaws.com/bsc.png',
+      key: 'binance-smart-chain',
+      base_url: 'https://bscscan.com/token',
     },
     {
       name: 'Fantom',
@@ -69,18 +122,27 @@ const Explorers = ({ platforms }) => {
     {
       name: 'Arbitrum-one',
       image: 'https://chain-icons.s3.amazonaws.com/arbitrum.png',
+
+      key: 'arbitrum',
+      base_url: 'https://arbiscan.io/token',
     },
     {
       name: 'Avalanche',
       image: 'https://chain-icons.s3.amazonaws.com/avalanche.png',
       key: 'avalanche',
-      base_url: 'https://cchain.explorer.avax.network/address/',
+      base_url: 'https://snowscan.xyz/token',
     },
     {
       name: 'Celo',
       image: 'https://chain-icons.s3.amazonaws.com/chainlist/42220',
       key: 'celo',
-      base_url: 'https://explorer.celo.org/address/',
+      base_url: 'https://celoscan.io/token',
+    },
+    {
+      name: 'Kava',
+      image:
+        'https://research.binance.com/static/images/projects/kava/logo.png',
+      key: 'kava',
     },
   ];
 
@@ -111,10 +173,16 @@ const Explorers = ({ platforms }) => {
           if (!explorer) return null;
 
           return (
-            <div
+            <Link
               key={index}
               className="mb-4 me-3"
-              style={{ cursor: 'pointer', minWidth: 180, overflow: 'hidden' }}
+              style={{
+                minWidth: 180,
+                overflow: 'hidden',
+                textDecoration: 'none',
+              }}
+              to={explorer.base_url ? `${explorer.base_url}/${address}` : '#'}
+              target={explorer.base_url ? '_blank' : '_self'}
             >
               <div className="w-100 p-1 py-2 border border-1 rounded d-flex align-items-center justify-content-start text-left">
                 <img
@@ -145,7 +213,7 @@ const Explorers = ({ platforms }) => {
                   </div>
                 </div>
               </div>
-            </div>
+            </Link>
           );
         })}
       </div>

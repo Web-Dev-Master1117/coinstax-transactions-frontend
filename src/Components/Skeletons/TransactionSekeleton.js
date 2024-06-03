508cb8643eae085f89d349c5730b287cdc6d5e2d
Web-Dev-Master1117 <remarkable.sr1117@gmail.com>
2024-06-03 16:02:36 -0300

    Update skeleton transactions

--- src/Components/Skeletons/TransactionSekeleton.js
+++ src/Components/Skeletons/TransactionSekeleton.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import ContentLoader from 'react-content-loader';
+import { useSelector } from 'react-redux';
+import { layoutModeTypes } from '../constants/layout';
 
 const TransactionSkeleton = (props) => {
-  // Calculamos los offsets base y las diferencias entre grupos
-  const baseY = 95;
+  const baseY = 90;
   const deltaY = 100;
+
+  const { layoutModeType } = useSelector((state) => ({
+    layoutModeType: state.Layout.layoutModeType,
+  }));
+
+  const darkMode = layoutModeType === layoutModeTypes['DARKMODE'];
 
   return (
     <ContentLoader
       height={1200}
       marginTop={10}
       width="100%"
-      backgroundColor="#333"
-      foregroundColor="#7f7f7f"
-      gradientRatio={0.5}
+      backgroundColor={darkMode ? '#333' : '#e0e0e0'}
+      foregroundColor={darkMode ? '#555' : '#cfcfcf'}
+      // gradientRatio={0.5}
+      // speed={2}
+      style={{
+        animation: 'fadeInOut 1s ease-in-out infinite',
+      }}
       {...props}
     >
-      {Array.from({ length: 12 }).map((_, index) => {
+      {Array.from({ length: 10 }).map((_, index) => {
         const y = baseY + index * deltaY;
 
         return (
@@ -29,12 +40,10 @@ const TransactionSkeleton = (props) => {
             <rect x="425" y={y - 16} rx="5" ry="5" width="150" height="12" />
             <rect x="425" y={y + 1} rx="5" ry="5" width="120" height="12" />
 
-            {/* <circle cx="690" cy={y} r="20" />
-            <rect x="735" y={y - 16} rx="5" ry="5" width="150" height="12" />
-            <rect x="735" y={y + 1} rx="5" ry="5" width="120" height="12" /> */}
-
             <rect x="1025" y={y - 16} rx="5" ry="5" width="100" height="12" />
             <rect x="1025" y={y + 1} rx="5" ry="5" width="100" height="12" />
+
+            <rect x="10" y={y + 35} rx="5" ry="5" width="1200" height="2" />
           </React.Fragment>
         );
       })}

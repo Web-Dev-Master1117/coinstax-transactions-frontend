96cd510c80bbac45353569a6d26e6e868e59a2db
Web-Dev-Master1117 <remarkable.sr1117@gmail.com>
2024-06-06 10:58:59 -0300

    feat: Improve responsiveness of AddressWithDropdown component

--- src/pages/DashboardInfo/components/ActivesTable.js
+++ src/pages/DashboardInfo/components/ActivesTable.js
@@ -6,6 +6,7 @@ import {
   DropdownItem,
   DropdownMenu,
   DropdownToggle,
+  Table,
 } from 'reactstrap';
 import {
   CurrencyUSD,
@@ -168,10 +169,10 @@ const ActivesTable = ({ data, loading, isDashboardPage }) => {
           <h1 className={` ms-1  mb-4 mt-4 `}>Assets</h1>
         </div>
       )}
-      <div>
-        <div
-          className={`${!isDashboardPage ? 'border rounded border-2 p-3' : ''}`}
-        >
+      <div
+        className={`${!isDashboardPage ? 'border rounded border-2 p-3' : ''}`}
+      >
+        <div>
           {!loading && (!data || !data.items || data.items?.length === 0) ? (
             <div className="text-center py-2 mt-3">
               <h4>No Assets Found</h4>
@@ -193,163 +194,176 @@ const ActivesTable = ({ data, loading, isDashboardPage }) => {
                   </div>
                 </div>
               )}
+              <div className="table-container ">
+                <div className="live-preview">
+                  <div className="table-responsive ">
+                    <Table className="table  table-borderless table-centered align-middle table-nowrap mb-0">
+                      <thead>
+                        <tr className="text-muted">
+                          <th scope="col">ASSETS</th>
+                          <th scope="col">PRICE</th>
+                          <th scope="col">BALANCE</th>
+                          <th scope="col">VALUE</th>
+                        </tr>
+                      </thead>
+                      {loading ? (
+                        <tr>
+                          <td colSpan="12" className="text-center">
+                            <tbody>
+                              <AssetsSkeleton />
+                            </tbody>
+                          </td>
+                        </tr>
+                      ) : displayItems.length === 0 ? (
+                        <tbody className="">
+                          <tr>
+                            <td colSpan="4" className="text-center pb-2 pt-5">
+                              <h4>No Assets Yet</h4>
+                            </td>
+                          </tr>
+                        </tbody>
+                      ) : (
+                        <tbody>
+                          {displayItems.map((asset, index) => (
+                            <tr key={index} className="">
+                              <td>
+                                <div className="d-flex align-items-center fw-high">
+                                  <img
+                                    src={asset.logo}
+                                    alt={asset.name}
+                                    className="rounded-circle avatar-xs me-2"
+                                    onError={(e) => {
+                                      e.target.onerror = null;
+                                      if (
+                                        !e.target.parentNode.querySelector(
+                                          '.img-assets-placeholder',
+                                        )
+                                      ) {
+                                        e.target.style.display = 'none';
 
-              <table className="table table-borderless">
-                <thead>
-                  <tr className="text-muted">
-                    <th scope="col">ASSETS</th>
-                    <th scope="col">PRICE</th>
-                    <th scope="col">BALANCE</th>
-                    <th scope="col">VALUE</th>
-                  </tr>
-                </thead>
-                {loading ? (
-                  <tr>
-                    <td colSpan="12" className="text-center">
-                      <tbody>
-                        <AssetsSkeleton />
-                      </tbody>
-                    </td>
-                  </tr>
-                ) : displayItems.length === 0 ? (
-                  <tbody className="">
-                    <tr>
-                      <td colSpan="4" className="text-center pb-2 pt-5">
-                        <h4>No Assets Yet</h4>
-                      </td>
-                    </tr>
-                  </tbody>
-                ) : (
-                  <tbody>
-                    {displayItems.map((asset, index) => (
-                      <tr key={index} className="">
-                        <td>
-                          <div className="d-flex align-items-center fw-high">
-                            <img
-                              src={asset.logo}
-                              alt={asset.name}
-                              className="rounded-circle avatar-xs me-2"
-                              onError={(e) => {
-                                e.target.onerror = null;
-                                if (
-                                  !e.target.parentNode.querySelector(
-                                    '.img-assets-placeholder',
-                                  )
-                                ) {
-                                  e.target.style.display = 'none';
+                                        const textNode =
+                                          document.createElement('div');
+                                        textNode.textContent = asset.name
+                                          ?.substring(0, 3)
+                                          .toUpperCase();
+                                        textNode.className =
+                                          'img-assets-placeholder avatar-xs me-2';
 
-                                  const textNode =
-                                    document.createElement('div');
-                                  textNode.textContent = asset.name
-                                    ?.substring(0, 3)
-                                    .toUpperCase();
-                                  textNode.className =
-                                    'img-assets-placeholder avatar-xs me-2';
+                                        const container = e.target.parentNode;
+                                        container.insertBefore(
+                                          textNode,
+                                          container.firstChild,
+                                        );
+                                      }
+                                    }}
+                                  />
 
-                                  const container = e.target.parentNode;
-                                  container.insertBefore(
-                                    textNode,
-                                    container.firstChild,
-                                  );
-                                }
-                              }}
-                            />
-
-                            <div className="d-flex flex-column ">
-                              <div className="d-flex flex-row align-items-center">
-                                {asset.name}{' '}
-                                {viewMode === 'perPosition' && (
-                                  <Badge
-                                    color="soft-dark"
-                                    style={{ fontWeight: 'inherit' }}
-                                    className="mx-2 p-1 fs-7"
-                                  >
-                                    <span className="text-dark">
-                                      {asset.percentage < 1
-                                        ? '<0.01'
-                                        : asset.percentage}
-                                      {'%'}
-                                    </span>
-                                  </Badge>
+                                  <div className="d-flex flex-column ">
+                                    <div className="d-flex flex-row align-items-center">
+                                      {asset.name}{' '}
+                                      {viewMode === 'perPosition' && (
+                                        <Badge
+                                          color="soft-dark"
+                                          style={{ fontWeight: 'inherit' }}
+                                          className="mx-2 p-1 fs-7"
+                                        >
+                                          <span className="text-dark">
+                                            {asset.percentage < 1
+                                              ? '<0.01'
+                                              : asset.percentage}
+                                            {'%'}
+                                          </span>
+                                        </Badge>
+                                      )}
+                                    </div>
+                                    <div className="d-flex align-items-center text-muted">
+                                      <BlockchainImage
+                                        width={15}
+                                        height={15}
+                                        className={'me-1'}
+                                        blockchainType={asset.blockchain}
+                                      />
+                                      {asset.blockchain === 'bnb'
+                                        ? 'BNB Chain'
+                                        : capitalizeFirstLetter(
+                                            asset.blockchain,
+                                          )}{' '}
+                                      · Wallet
+                                    </div>
+                                  </div>
+                                </div>
+                              </td>
+                              <td>
+                                {asset.price
+                                  ? parseValuesToLocale(
+                                      asset.price,
+                                      CurrencyUSD,
+                                    )
+                                  : '$0.00'}
+                              </td>
+                              <td>
+                                {asset.balance ? (
+                                  <span>
+                                    {parseValuesToLocale(asset.balance, '') +
+                                      ' ' +
+                                      asset.symbol}
+                                  </span>
+                                ) : (
+                                  '0.00'
                                 )}
-                              </div>
-                              <div className="d-flex align-items-center text-muted">
-                                <BlockchainImage
-                                  width={15}
-                                  height={15}
-                                  className={'me-1'}
-                                  blockchainType={asset.blockchain}
-                                />
-                                {asset.blockchain === 'bnb'
-                                  ? 'BNB Chain'
-                                  : capitalizeFirstLetter(
-                                      asset.blockchain,
-                                    )}{' '}
-                                · Wallet
-                              </div>
-                            </div>
-                          </div>
-                        </td>
-                        <td>
-                          {asset.price
-                            ? parseValuesToLocale(asset.price, CurrencyUSD)
-                            : '$0.00'}
-                        </td>
-                        <td>
-                          {asset.balance ? (
-                            <span>
-                              {parseValuesToLocale(asset.balance, '') +
-                                ' ' +
-                                asset.symbol}
-                            </span>
-                          ) : (
-                            '0.00'
-                          )}
-                        </td>
-                        <td>
-                          <div className="d-flex flex-column align-items-start">
-                            <span>
-                              {asset.value
-                                ? parseValuesToLocale(asset.value, CurrencyUSD)
-                                : parseValuesToLocale(0, CurrencyUSD)}
-                            </span>
-                            <small
-                              className={`${
-                                asset.prettyDeltaValuePercent === '0.00%'
-                                  ? 'text-primary'
-                                  : asset.prettyDeltaValuePercent[0] === '-'
-                                    ? 'text-danger'
-                                    : 'text-success'
-                              }`}
-                            >
-                              {asset.prettyDeltaValuePercent === '0.00%'
-                                ? parseValuesToLocale(
-                                    asset.deltaValuePercent,
-                                    '',
-                                  )
-                                : (asset.prettyDeltaValuePercent[0] === '-'
-                                    ? ''
-                                    : '+') +
-                                  parseValuesToLocale(
-                                    asset.deltaValuePercent,
-                                    '',
-                                  )}
-                              {asset.deltaValue
-                                ? ' (' +
-                                  parseValuesToLocale(
-                                    asset.deltaValue,
-                                    CurrencyUSD,
-                                  ) +
-                                  ')'
-                                : null}
-                            </small>
-                          </div>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                )}
-              </table>
+                              </td>
+                              <td>
+                                <div className="d-flex flex-column align-items-start">
+                                  <span>
+                                    {asset.value
+                                      ? parseValuesToLocale(
+                                          asset.value,
+                                          CurrencyUSD,
+                                        )
+                                      : parseValuesToLocale(0, CurrencyUSD)}
+                                  </span>
+                                  <small
+                                    className={`${
+                                      asset.prettyDeltaValuePercent === '0.00%'
+                                        ? 'text-primary'
+                                        : asset.prettyDeltaValuePercent[0] ===
+                                            '-'
+                                          ? 'text-danger'
+                                          : 'text-success'
+                                    }`}
+                                  >
+                                    {asset.prettyDeltaValuePercent === '0.00%'
+                                      ? parseValuesToLocale(
+                                          asset.deltaValuePercent,
+                                          '',
+                                        )
+                                      : (asset.prettyDeltaValuePercent[0] ===
+                                        '-'
+                                          ? ''
+                                          : '+') +
+                                        parseValuesToLocale(
+                                          asset.deltaValuePercent,
+                                          '',
+                                        )}
+                                    {asset.deltaValue
+                                      ? ' (' +
+                                        parseValuesToLocale(
+                                          asset.deltaValue,
+                                          CurrencyUSD,
+                                        ) +
+                                        ')'
+                                      : null}
+                                  </small>
+                                </div>
+                              </td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      )}
+                    </Table>
+                  </div>
+                </div>
+              </div>
             </div>
           )}
         </div>

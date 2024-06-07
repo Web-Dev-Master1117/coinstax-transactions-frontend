97d447d11c586cd42a8ff7e925c12a09b91396cb
Web-Dev-Master1117 <remarkable.sr1117@gmail.com>
2024-06-07 10:28:12 -0300

    Responsive styles in transactions

--- src/pages/DashboardTransactions/HistorialComponents/components/Columns/BlockChainActionColumn.js
+++ src/pages/DashboardTransactions/HistorialComponents/components/Columns/BlockChainActionColumn.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Badge, Col } from 'reactstrap';
+import { Badge, Col, Row } from 'reactstrap';
 import eth from '../../../../../assets/images/svg/crypto-icons/eth.svg';
 
 import ValueColumn from './ValueColumn';
@@ -20,7 +20,9 @@ const BlockChainActionColumn = ({ transaction }) => {
     <>
       <Col
         className={`d-flex ${
-          transaction.successful && !transaction.successful ? 'col-12' : 'col-7'
+          transaction.successful && !transaction.successful
+            ? 'col-12'
+            : 'col-lg-6 col-xl-6   col-8 '
         }`}
       >
         {transaction.blockchainAction && (
@@ -40,17 +42,7 @@ const BlockChainActionColumn = ({ transaction }) => {
                 getActionMapping(transaction.blockchainAction).icon
               } fs-2`}
             ></i>
-            {/* <img
-              src={eth}
-              alt="Ethereum"
-              className="position-absolute"
-              style={{
-                bottom: '-3px',
-                right: '-2px',
-                width: '15px',
-                height: '15px',
-              }}
-            /> */}
+
             <BlockchainImage
               style={{
                 bottom: '-3px',
@@ -72,15 +64,43 @@ const BlockChainActionColumn = ({ transaction }) => {
           <p className="text-start my-0">{formatTime(transaction.date)}</p>
         </div>
       </Col>
-      <Col className=" w-100">
+      <Col className="col-4 col-xl-6 col-lg-6 ms-lg-o ms-xl-0 ms-md-3  ms-4 ">
         {transaction.successful ? (
           transaction.txSummary.value && (
-            <div className="d-flex text-start justify-content-start ms-n2">
+            <div className="d-flex text-start justify-content-start ">
+              <div
+                style={{
+                  marginLeft: '1.35rem',
+                }}
+              >
+                {''}
+              </div>
+              <img
+                src=""
+                alt=""
+                className="rounded  img-transaction"
+                width="auto"
+                height={35}
+              />
               <ValueColumn transaction={transaction} />
             </div>
           )
         ) : (
-          <div className="ms-n2">
+          <div className="ms-sm-4 ms-3">
+            <div
+              style={{
+                marginLeft: '1.35rem',
+              }}
+            >
+              {''}
+            </div>
+            <img
+              src=""
+              alt=""
+              className="rounded img-transaction"
+              width="auto"
+              height={35}
+            />
             <Badge color="soft-danger" className="rounded-pill">
               <div className="text-danger fw-normal p-0 d-flex align-items-center">
                 <i className="ri-close-line px-0 fs-5 p-0"></i>

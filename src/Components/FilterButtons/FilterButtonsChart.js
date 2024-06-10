8d06384be0b6043ce4b3b07598658c2ed286783d
Web-Dev-Master1117 <remarkable.sr1117@gmail.com>
2024-06-10 10:17:54 -0300

    Fix active filter state update in handleFilterForDays function

--- src/Components/FilterButtons/FilterButtonsChart.js
+++ src/Components/FilterButtons/FilterButtonsChart.js
@@ -8,17 +8,19 @@ const FilterButtonsChart = ({
   loading,
   activeFilter,
   handleFilterForDays,
-}) => (
-  <>
-    <button
-      disabled={loading || activeFilter === id}
-      onClick={() => handleFilterForDays(days, id)}
-      type="button"
-      className={`btn btn-soft-primary rounded-pill timeline-btn btn-sm ${activeFilter === id ? 'active' : ''}`}
-      id={id}
-    >
-      {label}
-    </button>
-  </>
-);
+}) => {
+  return (
+    <>
+      <button
+        disabled={loading || activeFilter === id}
+        onClick={() => handleFilterForDays(days, id)}
+        type="button"
+        className={`btn btn-soft-primary rounded-pill timeline-btn btn-sm ${activeFilter === id ? 'active' : ''}`}
+        id={id}
+      >
+        {label}
+      </button>
+    </>
+  );
+};
 export default FilterButtonsChart;

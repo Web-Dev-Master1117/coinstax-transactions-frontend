4b815decc59929ae8067920b143ce01d4d876771
Web-Dev-Master1117 <remarkable.sr1117@gmail.com>
2024-06-16 12:33:00 -0300

    - Assets table: calculate percent share of each assets based on address summary total value, rather than on a per-blockchain basis.

--- src/pages/DashboardAssets/components/AssestTable.js
+++ src/pages/DashboardAssets/components/AssestTable.js
@@ -30,6 +30,9 @@ const AssetsTable = ({
 
   const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 955);
 
+  const addressTotalValue = addressInfo?.blockchains?.all?.totalValue || 0;
+  const isLoadingAddressSummary = !addressInfo?.complete;
+
   useEffect(() => {
     const handleResize = () => {
       setIsSmallScreen(window.innerWidth < 955);
@@ -42,59 +45,77 @@ const AssetsTable = ({
   const columns = [
     {
       name: 'ASSETS',
-      selector: (row) => (
-        <div className="d-flex align-items-center fw-high">
-          <img
-            src={row.logo}
-            alt={row.name}
-            className="rounded-circle avatar-xs me-2"
-            onError={(e) => {
-              e.target.onerror = null;
-              if (
-                !e.target.parentNode.querySelector('.img-assets-placeholder')
-              ) {
-                e.target.style.display = 'none';
+      selector: (row) => {
+        // Calculate per position percent based on address total value. If it's laoding, show like it's loading the percent, or show the item percent.
+        const perPositionPercent = isLoadingAddressSummary
+          ? row.percentage
+          : (row.value / addressTotalValue) * 100;
 
-                const textNode = document.createElement('div');
-                textNode.textContent = row.name?.substring(0, 3).toUpperCase();
-                textNode.className = 'img-assets-placeholder avatar-xs me-2';
+        const finalPerPositionPercent =
+          perPositionPercent < 0.01 ? '<0.01' : perPositionPercent.toFixed(2);
 
-                const container = e.target.parentNode;
-                container.insertBefore(textNode, container.firstChild);
-              }
-            }}
-          />
-          <div className="d-flex flex-column">
-            <div className="d-flex flex-row align-items-center">
-              {row.name}{' '}
-              {viewMode === 'perPosition' && (
-                <Badge
-                  color="soft-light"
-                  style={{ fontWeight: 'inherit' }}
-                  className="mx-2 p-1 fs-7"
-                >
-                  <span className="text-dark">
-                    {row.percentage < 1 ? '<0.01' : row.percentage}
-                    {'%'}
-                  </span>
-                </Badge>
-              )}
-            </div>
-            <div className="d-flex align-items-center mt-1 text-muted">
-              <BlockchainImage
-                width={15}
-                height={15}
-                className={'me-1'}
-                blockchainType={row.blockchain}
-              />
-              {row.blockchain === 'bnb'
-                ? 'BNB Chain'
-                : capitalizeFirstLetter(row.blockchain)}{' '}
-              · Wallet
+        return (
+          <div className="d-flex align-items-center fw-high">
+            <img
+              src={row.logo}
+              alt={row.name}
+              className="rounded-circle avatar-xs me-2"
+              onError={(e) => {
+                e.target.onerror = null;
+                if (
+                  !e.target.parentNode.querySelector('.img-assets-placeholder')
+                ) {
+                  e.target.style.display = 'none';
+
+                  const textNode = document.createElement('div');
+                  textNode.textContent = row.name
+                    ?.substring(0, 3)
+                    .toUpperCase();
+                  textNode.className = 'img-assets-placeholder avatar-xs me-2';
+
+                  const container = e.target.parentNode;
+                  container.insertBefore(textNode, container.firstChild);
+                }
+              }}
+            />
+            <div className="d-flex flex-column">
+              <div
+                style={{ height: '18px' }}
+                className="d-flex flex-row align-items-center"
+              >
+                {row.name}{' '}
+                {viewMode === 'perPosition' && (
+                  <Badge
+                    color="soft-light"
+                    style={{ fontWeight: 'inherit' }}
+                    className="mx-2 p-1 fs-7"
+                  >
+                    <span className="text-dark">
+                      {isLoadingAddressSummary ? (
+                        '...'
+                      ) : (
+                        <>{finalPerPositionPercent}%</>
+                      )}
+                    </span>
+                  </Badge>
+                )}
+              </div>
+              <div className="d-flex align-items-center mt-1 text-muted">
+                <BlockchainImage
+                  width={15}
+                  height={15}
+                  className={'me-1'}
+                  blockchainType={row.blockchain}
+                />
+                {row.blockchain === 'bnb'
+                  ? 'BNB Chain'
+                  : capitalizeFirstLetter(row.blockchain)}{' '}
+                · Wallet
+              </div>
             </div>
           </div>
-        </div>
-      ),
+        );
+      },
       sortable: false,
       grow: 2,
     },
@@ -139,32 +160,33 @@ const AssetsTable = ({
           ? parseValuesToLocale(row.value, CurrencyUSD)
           : parseValuesToLocale(0, CurrencyUSD),
       sortable: false,
-      cell: (row) => (
-        <div className="value-cell flex-column d-flex">
-          <span>
-            {row.value
-              ? parseValuesToLocale(row.value, CurrencyUSD)
-              : parseValuesToLocale(0, CurrencyUSD)}
-          </span>
-          <small
-            className={`${
-              row.prettyDeltaValuePercent === '0.00%'
-                ? 'text-primary'
-                : row.prettyDeltaValuePercent[0] === '-'
-                  ? 'text-danger'
-                  : 'text-success'
-            }`}
-          >
-            {row.prettyDeltaValuePercent === '0.00%'
-              ? parseValuesToLocale(row.deltaValuePercent, '')
-              : (row.prettyDeltaValuePercent[0] === '-' ? '' : '+') +
+      cell: (row) => {
+        return (
+          <div className="value-cell flex-column d-flex">
+            <span>
+              {row.value
+                ? parseValuesToLocale(row.value, CurrencyUSD)
+                : parseValuesToLocale(0, CurrencyUSD)}
+            </span>
+            <small
+              className={`${row.prettyDeltaValuePercent === '0.00%'
+                  ? 'text-primary'
+                  : row.prettyDeltaValuePercent[0] === '-'
+                    ? 'text-danger'
+                    : 'text-success'
+                }`}
+            >
+              {row.prettyDeltaValuePercent === '0.00%'
+                ? parseValuesToLocale(row.deltaValuePercent, '')
+                : (row.prettyDeltaValuePercent[0] === '-' ? '' : '+') +
                 parseValuesToLocale(row.deltaValuePercent, '')}
-            {row.deltaValue
-              ? ' (' + parseValuesToLocale(row.deltaValue, CurrencyUSD) + ')'
-              : null}
-          </small>
-        </div>
-      ),
+              {row.deltaValue
+                ? ' (' + parseValuesToLocale(row.deltaValue, CurrencyUSD) + ')'
+                : null}
+            </small>
+          </div>
+        );
+      },
       grow: 1,
     },
   ];
@@ -201,6 +223,11 @@ const AssetsTable = ({
                 backgroundColor: `${isDarkMode ? '#16161a' : ''}`,
                 color: `${isDarkMode ? '#fff' : ''}`,
                 border: 'none',
+              },
+            },
+            noData: {
+              style: {
+                backgroundColor: `${isDarkMode ? '#16161a' : ''}`,
               },
             },
           }}

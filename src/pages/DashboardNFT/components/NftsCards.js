54565232751962f2dc4530df012483690af66479
Web-Dev-Master1117 <remarkable.sr1117@gmail.com>
2024-07-08 18:53:36 -0300

    - Fix for nfts cards, for spam filtering.

--- src/pages/DashboardNFT/components/NftsCards.js
+++ src/pages/DashboardNFT/components/NftsCards.js
@@ -47,6 +47,9 @@ const NftsCards = ({
       ).unwrap();
 
       if (response.spam !== undefined) {
+
+
+
         setNfts((prevNfts) =>
           prevNfts
             .map((nft) =>
@@ -54,7 +57,7 @@ const NftsCards = ({
                 ? { ...nft, isSpam: response.spam }
                 : nft,
             )
-            .filter((nft) => !nft.isSpam),
+          // .filter((nft) => !nft.isSpam),
         );
       }
     } catch (error) {
@@ -63,8 +66,10 @@ const NftsCards = ({
   };
 
   useEffect(() => {
-    setNfts(item.filter((nft) => !nft.isSpam));
+    setNfts(item);
   }, [item]);
+
+  const nftsToRender = includeSpam ? nfts : nfts.filter((nft) => !nft.isSpam) || []
 
   return (
     <div
@@ -75,185 +80,183 @@ const NftsCards = ({
         justifyContent: 'center',
       }}
     >
-      {nfts &&
-        nfts?.length > 0 &&
-        nfts?.map((nft, index) => {
-          const { floorPriceFiat, floorPriceNativeToken, isSpam } = nft;
-          const hasFiatFloorPrice =
-            floorPriceFiat && Number(floorPriceFiat) > 0;
-          const hasNativeTokenFloorPrice =
-            floorPriceNativeToken && Number(floorPriceNativeToken) > 0;
-          const hasFloorPrice = showFiatValues
-            ? hasFiatFloorPrice
-            : hasNativeTokenFloorPrice;
-          const floorPrice = showFiatValues
-            ? parseValuesToLocale(floorPriceFiat, CurrencyUSD)
-            : parseValuesToLocale(floorPriceNativeToken) +
-              `${nft?.nativeSymbol || ''}`;
-          const shouldShowUnsupported =
-            !nft.logo || imageErrors[nft.contractAddress + nft.tokenId];
-          const iconId = isSpam
-            ? `spam-icon-spam-${index}`
-            : `spam-icon-not-spam-${index}`;
-          return (
-            <div
-              key={index}
-              className="d-flex justify-content-center position-relative card-container"
+      {nftsToRender?.map((nft, index) => {
+        const { floorPriceFiat, floorPriceNativeToken, isSpam } = nft;
+        const hasFiatFloorPrice =
+          floorPriceFiat && Number(floorPriceFiat) > 0;
+        const hasNativeTokenFloorPrice =
+          floorPriceNativeToken && Number(floorPriceNativeToken) > 0;
+        const hasFloorPrice = showFiatValues
+          ? hasFiatFloorPrice
+          : hasNativeTokenFloorPrice;
+        const floorPrice = showFiatValues
+          ? parseValuesToLocale(floorPriceFiat, CurrencyUSD)
+          : parseValuesToLocale(floorPriceNativeToken) +
+          `${nft?.nativeSymbol || ''}`;
+        const shouldShowUnsupported =
+          !nft.logo || imageErrors[nft.contractAddress + nft.tokenId];
+        const iconId = isSpam
+          ? `spam-icon-spam-${index}`
+          : `spam-icon-not-spam-${index}`;
+        return (
+          <div
+            key={index}
+            className="d-flex justify-content-center position-relative card-container"
+          >
+            <div className="spam-flag cursor-pointer">
+              <span>
+                {isSpam ? (
+                  <div
+                    id={iconId}
+                    ref={(el) => (iconRefs.current[index] = el)}
+                    onClick={() =>
+                      handleSpam(nft.contractAddress, nft.tokenId, false)
+                    }
+                  >
+                    <i className="ri-spam-fill fs-4 p-0"></i>
+                    {tooltipTargetIds.includes(iconId) && (
+                      <UncontrolledTooltip
+                        placement="left"
+                        target={iconId}
+                        className="popover-dark"
+                      >
+                        <span style={{ fontSize: '0.70rem' }}>
+                          Unflag as Spam
+                        </span>
+                      </UncontrolledTooltip>
+                    )}
+                  </div>
+                ) : (
+                  <div
+                    id={iconId}
+                    ref={(el) => (iconRefs.current[index] = el)}
+                    onClick={() =>
+                      handleSpam(nft.contractAddress, nft.tokenId, true)
+                    }
+                  >
+                    <i className="ri-spam-line fs-4 p-0"></i>
+                    {tooltipTargetIds.includes(iconId) && (
+                      <UncontrolledTooltip
+                        placement="left"
+                        target={iconId}
+                        className="popover-dark"
+                      >
+                        <span style={{ fontSize: '0.70rem' }}>
+                          Flag as Spam
+                        </span>
+                      </UncontrolledTooltip>
+                    )}
+                  </div>
+                )}
+              </span>
+            </div>
+            <Card
+              onClick={() =>
+                handleVisitNFT(
+                  nft.contractAddress,
+                  nft.tokenId,
+                  nft.blockchain,
+                )
+              }
+              className="cursor-pointer border-2 border bg-transparent shadow-none"
+              style={{
+                borderRadius: '10px',
+                minWidth: '100%',
+              }}
             >
-              <div className="spam-flag cursor-pointer">
-                <span>
-                  {isSpam ? (
+              <CardHeader className="border-0 bg-transparent p-1">
+                <div
+                  style={{
+                    position: 'relative',
+                    minHeight: '200px',
+                  }}
+                >
+                  {shouldShowUnsupported ? (
                     <div
-                      id={iconId}
-                      ref={(el) => (iconRefs.current[index] = el)}
-                      onClick={() =>
-                        handleSpam(nft.contractAddress, nft.tokenId, false)
-                      }
+                      className="d-flex justify-content-center align-item-center"
+                      style={{
+                        maxWidth: '100%',
+                        maxHeight: '100%',
+                        aspectRatio: '1 / 1',
+                        objectFit: 'cover',
+                        borderRadius: '8px',
+                        backgroundColor: '',
+                      }}
                     >
-                      <i className="ri-spam-fill fs-4 p-0"></i>
-                      {tooltipTargetIds.includes(iconId) && (
-                        <UncontrolledTooltip
-                          placement="left"
-                          target={iconId}
-                          className="popover-dark"
-                        >
-                          <span style={{ fontSize: '0.70rem' }}>
-                            Unflag as Spam
-                          </span>
-                        </UncontrolledTooltip>
-                      )}
+                      <h3 className="text-center pt-5">
+                        Unsupported content
+                      </h3>
                     </div>
                   ) : (
-                    <div
-                      id={iconId}
-                      ref={(el) => (iconRefs.current[index] = el)}
-                      onClick={() =>
-                        handleSpam(nft.contractAddress, nft.tokenId, true)
+                    <img
+                      src={nft.logo}
+                      alt=""
+                      className="img-fluid w-100 position-realative"
+                      style={{
+                        maxWidth: '100%',
+                        maxHeight: '100%',
+                        aspectRatio: '1 / 1',
+                        objectFit: 'cover',
+                        borderRadius: '8px',
+                      }}
+                      onError={() =>
+                        setImageErrors((prev) => ({
+                          ...prev,
+                          [nft.contractAddress + nft.tokenId]: true,
+                        }))
                       }
-                    >
-                      <i className="ri-spam-line fs-4 p-0"></i>
-                      {tooltipTargetIds.includes(iconId) && (
-                        <UncontrolledTooltip
-                          placement="left"
-                          target={iconId}
-                          className="popover-dark"
-                        >
-                          <span style={{ fontSize: '0.70rem' }}>
-                            Flag as Spam
-                          </span>
-                        </UncontrolledTooltip>
-                      )}
-                    </div>
+                    />
                   )}
-                </span>
-              </div>
-              <Card
-                onClick={() =>
-                  handleVisitNFT(
-                    nft.contractAddress,
-                    nft.tokenId,
-                    nft.blockchain,
-                  )
-                }
-                className="cursor-pointer border-2 border bg-transparent shadow-none"
-                style={{
-                  borderRadius: '10px',
-                  minWidth: '100%',
-                }}
-              >
-                <CardHeader className="border-0 bg-transparent p-1">
-                  <div
-                    style={{
-                      position: 'relative',
-                      minHeight: '200px',
-                    }}
-                  >
-                    {shouldShowUnsupported ? (
-                      <div
-                        className="d-flex justify-content-center align-item-center"
+                  <div className="">
+                    <BlockchainImage
+                      blockchainType={nft.blockchain}
+                      style={{
+                        position: 'absolute',
+                        bottom: `${isDashboardPage ? '11%' : '7%'}`,
+                        left: '2%',
+                        width: 'auto',
+                        height: '10%',
+                      }}
+                      className="img-fluid border-dark border border-rounded border-1 d-flex justify-content-start shadow-md rounded-circle"
+                    />
+                  </div>
+                </div>
+              </CardHeader>
+              <CardBody className="pt-1">
+                <div
+                  className="d-flex flex-column justify-content-between"
+                  style={{ height: '100%' }}
+                >
+                  <div>
+                    {nft?.collection?.name ? (
+                      <span
                         style={{
-                          maxWidth: '100%',
-                          maxHeight: '100%',
-                          aspectRatio: '1 / 1',
-                          objectFit: 'cover',
-                          borderRadius: '8px',
-                          backgroundColor: '',
+                          fontSize: '11px',
+                          marginBottom: '6px',
+                          display: 'block',
                         }}
+                        className="text-dark"
                       >
-                        <h3 className="text-center pt-5">
-                          Unsupported content
-                        </h3>
-                      </div>
-                    ) : (
-                      <img
-                        src={nft.logo}
-                        alt=""
-                        className="img-fluid w-100 position-realative"
-                        style={{
-                          maxWidth: '100%',
-                          maxHeight: '100%',
-                          aspectRatio: '1 / 1',
-                          objectFit: 'cover',
-                          borderRadius: '8px',
-                        }}
-                        onError={() =>
-                          setImageErrors((prev) => ({
-                            ...prev,
-                            [nft.contractAddress + nft.tokenId]: true,
-                          }))
-                        }
-                      />
-                    )}
-                    <div className="">
-                      <BlockchainImage
-                        blockchainType={nft.blockchain}
-                        style={{
-                          position: 'absolute',
-                          bottom: `${isDashboardPage ? '11%' : '7%'}`,
-                          left: '2%',
-                          width: 'auto',
-                          height: '10%',
-                        }}
-                        className="img-fluid border-dark border border-rounded border-1 d-flex justify-content-start shadow-md rounded-circle"
-                      />
-                    </div>
-                  </div>
-                </CardHeader>
-                <CardBody className="pt-1">
-                  <div
-                    className="d-flex flex-column justify-content-between"
-                    style={{ height: '100%' }}
-                  >
-                    <div>
-                      {nft?.collection?.name ? (
-                        <span
-                          style={{
-                            fontSize: '11px',
-                            marginBottom: '6px',
-                            display: 'block',
-                          }}
-                          className="text-dark"
-                        >
-                          {nft.collection.name || ' '}
-                        </span>
-                      ) : null}
-
-                      <h6 style={{ fontSize: '14px' }} className="text-dark">
-                        {nft.name || ' '}
-                      </h6>
-                    </div>
-                    {hasFloorPrice ? (
-                      <div>
-                        <span>{hasFloorPrice ? 'Floor Price' : ' '}</span>
-                        <h6 className="text-dark d-flex mb-0">{floorPrice}</h6>
-                      </div>
+                        {nft.collection.name || ' '}
+                      </span>
                     ) : null}
+
+                    <h6 style={{ fontSize: '14px' }} className="text-dark">
+                      {nft.name || ' '}
+                    </h6>
                   </div>
-                </CardBody>
-              </Card>
-            </div>
-          );
-        })}
+                  {hasFloorPrice ? (
+                    <div>
+                      <span>{hasFloorPrice ? 'Floor Price' : ' '}</span>
+                      <h6 className="text-dark d-flex mb-0">{floorPrice}</h6>
+                    </div>
+                  ) : null}
+                </div>
+              </CardBody>
+            </Card>
+          </div>
+        );
+      })}
     </div>
   );
 };

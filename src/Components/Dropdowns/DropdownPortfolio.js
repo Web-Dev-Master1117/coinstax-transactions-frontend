3a1ccfd3ec403e132db02cafa9b25bfebe20453d
Web-Dev-Master1117 <remarkable.sr1117@gmail.com>
2024-07-30 15:26:11 -0300

    Sub menu options in portfolio dropdown

--- src/Components/Dropdowns/DropdownPortfolio.js
+++ src/Components/Dropdowns/DropdownPortfolio.js
@@ -6,6 +6,7 @@ import {
   DropdownToggle,
 } from 'reactstrap';
 import {
+  copyToClipboard,
   CurrencyUSD,
   formatIdTransaction,
   parseValuesToLocale,
@@ -13,14 +14,17 @@ import {
 import DropdownMenuPortal from './DropdownPortal';
 import { Link, useParams } from 'react-router-dom';
 import {
+  deleteUserAddressWallet,
   getPortfolioWallets,
   getUserWallets,
+  updateUserWalletAddress,
 } from '../../slices/userWallets/thunk';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { DASHBOARD_USER_ROLES } from '../../common/constants';
 import Skeleton from 'react-loading-skeleton';
 import { layoutModeTypes } from '../constants/layout';
+import Swal from 'sweetalert2';
 
 const DropdownPortfolio = ({ dropdownOpen, toggleDropdown }) => {
   const dispatch = useDispatch();
@@ -116,6 +120,173 @@ const DropdownPortfolio = ({ dropdownOpen, toggleDropdown }) => {
     toggleDropdown();
   };
 
+  const [subDropdownOpen, setSubDropdownOpen] = useState(null);
+
+  const toggleSubDropdown = (e, index) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (subDropdownOpen === index) {
+      setSubDropdownOpen(null);
+    } else {
+      setSubDropdownOpen(index);
+    }
+  };
+
+  const handleUpdateAddress = (e, address) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    Swal.fire({
+      title: 'Update Wallet Address',
+      input: 'text',
+      inputValue: address.Name,
+      showCancelButton: true,
+      confirmButtonText: 'Save',
+      inputValidator: (value) => {
+        if (
+          userPortfolio.some(
+            (addr) => addr.Name === value && addr.Address !== address.Address,
+          )
+        ) {
+          return 'This name already exists!';
+        }
+      },
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const newName = result.value.trim() ? result.value : null;
+
+        try {
+          const response = await dispatch(
+            updateUserWalletAddress({
+              userId,
+              name: newName,
+              addressId: address.Id,
+            }),
+          ).unwrap();
+
+          if (response && !response.error) {
+            // Swal.fire({
+            //   title: 'Success',
+            //   text: 'Wallet address updated successfully',
+            //   icon: 'success',
+            // });
+
+            fetchUserWallets();
+          } else {
+            Swal.fire({
+              title: 'Error',
+              text: 'Failed to update address',
+              icon: 'error',
+            });
+          }
+        } catch (error) {
+          Swal.fire({
+            title: 'Error',
+            text: 'Failed to update address',
+            icon: 'error',
+          });
+
+          console.log(error);
+        }
+      }
+    });
+  };
+
+  const handleDeleteUserAddress = (address) => {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: `Are you sure to delete wallet ${address.Name ? address.Name : address.Address}?`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Delete',
+      cancelButtonText: 'Cancel',
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          const response = await dispatch(
+            deleteUserAddressWallet({ userId, addressId: address.Id }),
+          ).unwrap();
+
+          if (response && !response.error) {
+            Swal.fire({
+              title: 'Success',
+              text: 'Wallet address deleted successfully',
+              icon: 'success',
+            });
+            fetchUserWallets();
+          } else {
+            Swal.fire({
+              title: 'Error',
+              text: 'Failed to delete address',
+              icon: 'error',
+            });
+          }
+        } catch (error) {
+          console.error('Failed to delete address:', error);
+          Swal.fire({
+            title: 'Error',
+            text: 'Failed to delete address',
+            icon: 'error',
+          });
+        }
+      }
+    });
+  };
+
+  const handleCopy = (e, text) => {
+    e.preventDefault();
+    e.stopPropagation();
+    copyToClipboard(text);
+    // Swal.fire({
+    //   text: 'Address copied to clipboard!',
+    //   icon: 'success',
+    //   timer: 2000,
+    //   showConfirmButton: false,
+    // });
+  };
+
+  const renderOptionsSubDropdown = (index, address) => {
+    return (
+      <Dropdown
+        isOpen={subDropdownOpen === index}
+        toggle={(e) => {
+          toggleSubDropdown(e, index);
+        }}
+        direction="right"
+      >
+        <DropdownToggle
+          caret={false}
+          className="btn btn-light btn-sm text-muted"
+          onClick={(e) => {
+            toggleSubDropdown(e, index);
+          }}
+        >
+          <i className="ri-more-2-fill"></i>
+        </DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem
+            className="d-flex align-items-center"
+            onClick={(e) => handleCopy(e, address.Address)}
+          >
+            <i className="ri-file-copy-line me-2"></i> Copy Address
+          </DropdownItem>
+          <DropdownItem
+            className="d-flex align-items-center"
+            onClick={(e) => handleUpdateAddress(e, address)}
+          >
+            <i className="ri-edit-line me-2"></i> Rename
+          </DropdownItem>
+          <DropdownItem
+            className="d-flex align-items-center"
+            onClick={() => handleDeleteUserAddress(address)}
+          >
+            <i className="ri-delete-bin-line me-2"></i> Delete
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    );
+  };
+
   return (
     <Dropdown className="ms-2" isOpen={dropdownOpen} toggle={toggleDropdown}>
       <DropdownToggle
@@ -171,48 +342,51 @@ const DropdownPortfolio = ({ dropdownOpen, toggleDropdown }) => {
           <div className="dropdown-divider"></div>
           {userPortfolio &&
             userPortfolio.map((address, index) => (
-              <DropdownItem
-                className="d-flex align-items-center"
-                key={index}
-                onClick={() => handleSelectAddress(address)}
-              >
-                <Link
-                  to={process.env.PUBLIC_URL + `/address/${address.Address}`}
-                  className="dropdown-item ps-0"
+              <>
+                <DropdownItem
+                  className="d-flex align-items-center"
+                  key={index}
+                  onClick={() => handleSelectAddress(address)}
                 >
-                  <div className="d-flex align-items-center">
-                    <i className="ri-link text-muted fs-3 align-middle me-3"></i>
-                    <div className="d-flex flex-column">
-                      <span className="align-middle">
+                  <Link
+                    to={process.env.PUBLIC_URL + `/address/${address.Address}`}
+                    className="dropdown-item ps-0"
+                  >
+                    <div className="d-flex align-items-center">
+                      <i className="ri-link text-muted fs-3 align-middle me-3"></i>
+                      <div className="d-flex flex-column">
+                        <span className="align-middle">
+                          {loadingWallets ? (
+                            <Skeleton
+                              width={80}
+                              baseColor={isDarkMode ? '#333' : '#f3f3f3'}
+                              highlightColor={isDarkMode ? '#444' : '#e0e0e0'}
+                            />
+                          ) : address.Name ? (
+                            address.Name
+                          ) : (
+                            formatIdTransaction(address.Address, 3, 6)
+                          )}
+                        </span>
                         {loadingWallets ? (
                           <Skeleton
-                            width={80}
+                            width={60}
                             baseColor={isDarkMode ? '#333' : '#f3f3f3'}
                             highlightColor={isDarkMode ? '#444' : '#e0e0e0'}
                           />
-                        ) : address.Name ? (
-                          address.Name
                         ) : (
-                          formatIdTransaction(address.Address, 3, 6)
+                          address.Name && (
+                            <span className="text-muted">
+                              {formatIdTransaction(address.Address, 3, 6)}
+                            </span>
+                          )
                         )}
-                      </span>
-                      {loadingWallets ? (
-                        <Skeleton
-                          width={60}
-                          baseColor={isDarkMode ? '#333' : '#f3f3f3'}
-                          highlightColor={isDarkMode ? '#444' : '#e0e0e0'}
-                        />
-                      ) : (
-                        address.Name && (
-                          <span className="text-muted">
-                            {formatIdTransaction(address.Address, 3, 6)}
-                          </span>
-                        )
-                      )}
+                      </div>
                     </div>
-                  </div>
-                </Link>
-              </DropdownItem>
+                  </Link>
+                  {renderOptionsSubDropdown(index, address)}
+                </DropdownItem>
+              </>
             ))}
           {userPortfolio.length > 0 && <div className="dropdown-divider"></div>}
           <DropdownItem>

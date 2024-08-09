67ab26485f4b0c5005a3922da73ef5c2eace31e3
Web-Dev-Master1117 <remarkable.sr1117@gmail.com>
2024-08-09 16:16:54 -0300

    Fix conditional request logic in Chart for Portfolio  endpoint

--- src/slices/portfolio/thunk.js
+++ src/slices/portfolio/thunk.js
@@ -54,25 +54,32 @@ export const fetchNFTSPortfolio = createAsyncThunk(
 
 export const getBalancesPortfolio = createAsyncThunk(
   'portfolio/getBalancesPortfolio',
-  async ({ userId, blockchain, signal }, { rejectWithValue }) => {
+  async ({ userId, blockchain, days, signal }, { rejectWithValue }) => {
     const token = getTokenFromCookies();
+    let url = `${API_BASE}/users/${userId}/portfolio/${blockchain}/balances`;
+
+    if (days) {
+      url += `?days=${days}`;
+    }
+
     try {
-      const response = await fetch(
-        `${API_BASE}/users/${userId}/portfolio/${blockchain}balances`,
-        {
-          headers: {
-            Authorization: `${token}`,
-          },
-          signal,
+      const response = await fetch(url, {
+        headers: {
+          Authorization: `${token}`,
         },
-      );
+        signal,
+      });
       if (!response.ok) {
         throw new Error(`Error: ${response.status}`);
       }
       const data = await response.json();
       return data;
     } catch (error) {
-      return rejectWithValue(error.message);
+      if (error.name === 'AbortError') {
+        console.log('Fetch aborted');
+      } else {
+        return rejectWithValue(error.message);
+      }
     }
   },
 );
